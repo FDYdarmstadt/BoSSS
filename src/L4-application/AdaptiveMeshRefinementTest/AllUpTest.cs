8c470b184e252d3cbed3ed2526d7a9ff41fa238c
florian Kummer <kummer@fdy.tu-darmstadt.de>
2017-10-25 16:58:14 +0200

    AMR: added test

---
+++ src/L4-application/AdaptiveMeshRefinementTest/AllUpTest.cs
@@ -0,0 +1,106 @@
+﻿using BoSSS.Solution;
+using MPI.Wrappers;
+using NUnit.Framework;
+using System;
+
+namespace BoSSS.Application.LoadBalancingTest {
+
+    /// <summary>
+    /// Complete Test for the load balancing.
+    /// </summary>
+    [TestFixture]
+    static public class AllUpTest {
+
+        /// <summary>
+        /// MPI init
+        /// </summary>
+        [TestFixtureSetUp]
+        public static void SetUp() {
+            bool MpiInit;
+            ilPSP.Environment.Bootstrap(
+                new string[0],
+                BoSSS.Solution.Application.GetBoSSSInstallDir(),
+                out MpiInit);
+        }
+
+        /// <summary>
+        /// Da Test!
+        /// </summary>
+        [Test]
+        static public void NoDynamicBalanceTest([Values(1, 2)] int DGdegree) {
+            LoadBalancingTestMain p = null;
+
+            BoSSS.Solution.Application._Main(
+                new string[0],
+                true,
+                null,
+                delegate() {
+                    p = new LoadBalancingTestMain();
+                    p.DynamicBalance = false;
+                    p.DEGREE = DGdegree;
+                    p.cellCostEstimatorFactory = CellCostEstimatorLibrary.MeasureCostOfWhateverFlorianThinksIsRelevantButIDontUnderstand;
+                    return p;
+                });
+        }
+
+        /// <summary>
+        /// Da Test!
+        /// </summary>
+        [Test]
+        static public void RuntimeCostDynamicBalanceTest(
+            [Values(1, 2)] int DGdegree) {
+            LoadBalancingTestMain p = null;
+            
+            BoSSS.Solution.Application._Main(
+                new string[0],
+                true,
+                null,
+                delegate () {
+                    p = new LoadBalancingTestMain();
+                    p.DynamicBalance = true;
+                    p.DEGREE = DGdegree;
+                    p.cellCostEstimatorFactory = CellCostEstimatorLibrary.MeasureCostOfWhateverFlorianThinksIsRelevantButIDontUnderstand;
+                    return p;
+                });
+        }
+
+        /// <summary>
+        /// Da Test!
+        /// </summary>
+        [Test]
+        static public void StaticCostDynamicBalanceTest(
+            [Values(1, 2)] int DGdegree) {
+            LoadBalancingTestMain p = null;
+
+            BoSSS.Solution.Application._Main(
+                new string[0],
+                true,
+                null,
+                delegate () {
+                    p = new LoadBalancingTestMain();
+                    p.DynamicBalance = true;
+                    p.DEGREE = DGdegree;
+                    p.cellCostEstimatorFactory = delegate(int performanceClassCount) {
+                        if (performanceClassCount != 2) {
+                            throw new Exception();
+                        }
+
+                        int[] performanceClassToCostMap = new int[] {
+                            1, // normal cells
+                            10 // assume cut cells cost 10 times more
+                        };
+                        return new StaticCellCostEstimator(performanceClassToCostMap);
+                    };
+                    return p;
+                });
+        }
+
+        /// <summary>
+        /// MPI shutdown.
+        /// </summary>
+        [TestFixtureTearDown]
+        public static void TestFixtureTearDown() {
+            csMPI.Raw.mpiFinalize();
+        }
+    }
+}
