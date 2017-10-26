9803f110feda0ab2a028086544bb865e85f48fc9
florian Kummer <kummer@fdy.tu-darmstadt.de>
2017-10-26 17:36:49 +0200

    AMR: first hierarchical refinment test

--- src/L4-application/AdaptiveMeshRefinementTest/AdaptiveMeshRefinementTestMain.cs
+++ src/L4-application/AdaptiveMeshRefinementTest/AdaptiveMeshRefinementTestMain.cs
@@ -3,6 +3,7 @@ using BoSSS.Foundation.Grid;
 using BoSSS.Foundation.Grid.Classic;
 using BoSSS.Foundation.IO;
 using BoSSS.Foundation.XDG;
+using BoSSS.Platform.Utils.Geom;
 using BoSSS.Solution;
 using BoSSS.Solution.Multigrid;
 using BoSSS.Solution.Tecplot;
@@ -24,8 +25,73 @@ namespace BoSSS.Application.AdaptiveMeshRefinementTest {
     /// </summary>
     class AdaptiveMeshRefinementTestMain : BoSSS.Solution.Application {
 
+        static void RefineScheisse() {
+            double[] nodes = GenericBlas.Linspace(-5, 5, 7);
+
+            BoundingBox BL = new BoundingBox(new[] { new[] { -10.0, -10.0 }, new[] { 0.0, 0.0 } });
+            GridCommons grd = Grid2D.Cartesian2DGrid(nodes, nodes, CutOuts: new BoundingBox[] { BL });
+            GridData gdat = new GridData(grd);
+
+            SinglePhaseField u = new SinglePhaseField(new Basis(gdat, 0), "u");
+            Tecplot.PlotFields(new DGField[] { u }, "Refinment-0", 0.0, 0);
+
+            int MaxRefinement = 5;
+
+            for(int iLevel = 0; iLevel <= MaxRefinement; iLevel++) {
+                Console.WriteLine("Level " + iLevel);
+
+                HashSet<int> CellsToRefine = new HashSet<int>();
+                int J = gdat.Cells.NoOfLocalUpdatedCells;
+                for(int j = 0; j < J; j++) {
+                    double xc = gdat.Cells.CellCenter[j, 0];
+                    double yc = gdat.Cells.CellCenter[j, 1];
+                    int RefinementLevel = gdat.Cells.GetCell(j).RefinementLevel;
+
+                    double r = Math.Sqrt(xc * xc + yc * yc);
+
+                    if((RefinementLevel < MaxRefinement) && (r < gdat.Cells.h_minGlobal*2)) {
+                        CellsToRefine.Add(j);
+                        RefineNeighboursRecursive(gdat, CellsToRefine, j, RefinementLevel);
+
+                    }
+                }
+
+                Console.Write("  refine: ");
+                foreach(int j in CellsToRefine) {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+
+
+                grd = gdat.Adapt(CellsToRefine.ToArray(), null);
+                gdat = new GridData(grd);
+                u = new SinglePhaseField(new Basis(gdat, 0), "u");
+                Tecplot.PlotFields(new DGField[] { u }, "Refinment-" + iLevel, 0.0, 0);
+
+            }
+        }
+
+        static void RefineNeighboursRecursive(GridData gdat, HashSet<int> CellsToRefine, int j, int DesiredLevel) {
+
+            foreach(var jNeigh in gdat.Cells.CellNeighbours[j]) {
+                var cl = gdat.Cells.GetCell(j);
+                if(cl.RefinementLevel < DesiredLevel) {
+                    CellsToRefine.Add(jNeigh);
+                    RefineNeighboursRecursive(gdat, CellsToRefine, jNeigh, cl.RefinementLevel);
+                }
+            }
+
+        }
+
+
+
         static void Main(string[] args) {
-            XQuadFactoryHelper.CheckQuadRules = true;
+            bool MpiInit;
+            ilPSP.Environment.Bootstrap(new string[0], BoSSS.Solution.Application.GetBoSSSInstallDir(), out MpiInit);
+
+            RefineScheisse();
+
+            return;
 
             BoSSS.Solution.Application._Main(
                 args,
