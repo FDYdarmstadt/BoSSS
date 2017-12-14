f16a9bcd1d0bdcbbf21f75bdc5f23e03b3cec814
Markus Geisenhofer <geisenhofer@fdy.tu-darmstadt.de>
2017-12-14 10:30:47 +0100

    ILPSP 3rd party: Changed folder names in PARMETIS folder and fixed build scripts

---
+++ src/ilPSP/layer_0/3rd_party/ParMETIS/ParMETIS/parmetislib.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 1997, Regents of the University of Minnesota
+ *
+ * par_metis.h
+ *
+ * This file includes all necessary header files
+ *
+ * Started 8/27/94
+ * George
+ *
+ * $Id: parmetislib.h,v 1.2 2003/07/21 17:50:22 karypis Exp $
+ */
+
+/*
+#define DEBUG			1
+#define DMALLOC			1
+*/
+
+#include "stdheaders.h"
+#include "parmetis.h"
+
+/* added by fkummer ****************************************************************/
+
+// surpress "deprecated"-warning
+#pragma warning (disable: 4996) 
+// surpress "unreference local variable"-warning
+#pragma warning (disable: 4101)
+// surpress "conversion from ... to ..., possible loss of data"-warning
+#pragma warning (disable: 4244)
+// surpress "conversion from ... to ..., possible loss of data"-warning
+#pragma warning (disable: 4305)
+
+/***************************************************************** end fkummer ****/
+
+#ifdef DMALLOC
+#include <dmalloc.h>
+#endif
+
+#include "rename.h"
+#include "defs.h"
+#include "struct.h"
+#include "macros.h"
+#include "proto.h"
+//#include "..\METIS\proto.h"
+
