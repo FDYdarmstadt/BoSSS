6e6370db62a5cfe16170f08161d962003d451533
Dennis Krause <krause@fdy.tu-darmstadt.de>
2018-01-09 17:18:31 +0100

    Merged

--- src/L2-foundation/BoSSS.Foundation/GridData_Refine.cs
+++ src/L2-foundation/BoSSS.Foundation/GridData_Refine.cs
@@ -44,6 +44,12 @@ namespace BoSSS.Foundation.Grid.Classic {
                 GridCommons oldGrid = this.m_Grid;
                 GridCommons newGrid = new GridCommons(oldGrid.RefElements, oldGrid.EdgeRefElements);
 
+                // Dirty hack for edge tags
+                newGrid.EdgeTagNames.Clear();
+                for (int i = 0; i < oldGrid.EdgeTagNames.Count; i++) {
+                    newGrid.EdgeTagNames.Add(oldGrid.EdgeTagNames.ElementAt(i));
+                }
+
                 Old2New = new GridCorrelation();
                                 
                 int J = this.Cells.NoOfLocalUpdatedCells;
@@ -110,7 +116,7 @@ namespace BoSSS.Foundation.Grid.Classic {
                         CellsToRefineBitmask[jCell] = true;
 
                         int[] Neighs, dummy;
-                        this.GetCellNeighbours(jCell, GetCellNeighbours_Mode.ViaEdges, out Neighs, out dummy);
+                        this.GetCellNeighbours(jCell, GetCellNeighbours_Mode.ViaVertices, out Neighs, out dummy);
 
                         foreach(int jNeigh in Neighs) {
                             AdaptNeighborsBitmask[jNeigh] = true;
@@ -160,7 +166,7 @@ namespace BoSSS.Foundation.Grid.Classic {
                                 throw new ArgumentException("Mismatch of 'CoarseningClusterID' within cluster.");
 
                             int[] Neighs, dummy;
-                            this.GetCellNeighbours(j, GetCellNeighbours_Mode.ViaEdges, out Neighs, out dummy);
+                            this.GetCellNeighbours(j, GetCellNeighbours_Mode.ViaVertices, out Neighs, out dummy);
 
                             foreach(int jNeigh in Neighs) {
                                 if(Array.IndexOf(jCellS, jNeigh) < 0) {
