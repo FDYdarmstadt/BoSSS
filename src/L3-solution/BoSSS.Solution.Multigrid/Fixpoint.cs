1165ad9c9b87e19fbf6e9309271191b1eeb5879a
Martin Smuda <smuda@fdy.tu-darmstadt.de>
2018-01-10 17:01:18 +0100

    moved handling of coupled iteration into CoupledIterationCallback

--- src/L3-solution/BoSSS.Solution.Multigrid/Fixpoint.cs
+++ src/L3-solution/BoSSS.Solution.Multigrid/Fixpoint.cs
@@ -55,13 +55,11 @@ namespace BoSSS.Solution.Multigrid {
 
         public double UnderRelax = 1.0;
 
-        /// <summary>
-        /// delegate for a coupled iteration which should be performed when the main solver converged
-        /// </summary>
-        /// <param name="locCurSt"></param>
-        public delegate void DelCoupledIteration(DGField[] locCurSt);
 
-        public DelCoupledIteration CoupledIteration;
+        ///// <summary>
+        ///// delays the evaluation of the coupled instance (e.g. Level-set) during update
+        ///// </summary>
+        //public event Action DelayCoupledIteration;
 
         /// <summary>
         /// delegate for checking the convergence criteria of the coupled iteration
@@ -73,28 +71,6 @@ namespace BoSSS.Solution.Multigrid {
 
 
         override public void SolverDriver<S>(CoordinateVector SolutionVec, S RHS) {
-
-            // setUp coupled iteration
-            // ========================
-
-            int NoOfCoupledIter = 0;
-            if (CoupledIteration != null) {
-                //if (CoupledIteration_Converged == null) {
-                //    throw new ArgumentNullException("No Convergence criteria defined for coupled iteration");
-                //}
-
-                NoOfCoupledIter++;
-                Console.WriteLine("Coupled Iteration {0}:", NoOfCoupledIter);
-
-                this.CoupledIteration(SolutionVec.Mapping.Fields.ToArray());
-
-            }
-
-            if (CoupledIteration_Converged == null) {
-                CoupledIteration_Converged = delegate () {
-                    return true;
-                };
-            }
 
 
             // initial guess and its residual
@@ -109,13 +85,16 @@ namespace BoSSS.Solution.Multigrid {
 
             OnIterationCallback(NoOfIterations, Solution.CloneAs(), Residual.CloneAs(), this.CurrentLin);
 
+            if (CoupledIteration_Converged == null)
+                CoupledIteration_Converged = delegate () {
+                    return true;
+                };
 
             // iterate...
             // ==========
             //int NoOfMainIterations = 0;
             while ((!(ResidualNorm < ConvCrit && CoupledIteration_Converged()) && NoOfIterations < MaxIter) || (NoOfIterations < MinIter)) {
                 NoOfIterations++;
-                //NoOfMainIterations++;
 
                 //DirectSolver ds = new DirectSolver();
                 //ds.Init(this.CurrentLin);
@@ -134,22 +113,6 @@ namespace BoSSS.Solution.Multigrid {
                 // (and for Level-Set-Updates ...)
                 this.CurrentLin.TransformSolFrom(SolutionVec, Solution);
 
-
-                // coupled iteration
-                // ==================
-
-                if (CoupledIteration != null && ResidualNorm < ConvCrit && !CoupledIteration_Converged()) {
-                    NoOfCoupledIter++;
-                    Console.WriteLine("Coupled Iteration {0}:", NoOfCoupledIter);
-
-                    this.CoupledIteration(SolutionVec.Mapping.Fields.ToArray());
-
-                    //NoOfMainIterations = 0;
-                }
-
-                // ==================
-
-
                 // update linearization
                 base.Update(SolutionVec.Mapping.Fields, ref Solution);
 
@@ -161,248 +124,7 @@ namespace BoSSS.Solution.Multigrid {
 
         }
 
-
-        // old version
-        //override public void SolverDriver<S>(CoordinateVector SolutionVec, S RHS) {
-
-
-        //    // initial guess and its residual
-        //    // ==============================
-        //    double[] Solution, Residual;
-        //    base.Init(SolutionVec, RHS, out Solution, out Residual);
-        //    double[] Correction = new double[Solution.Length];
-        //    double ResidualNorm = Residual.L2NormPow2().MPISum().Sqrt();
-        //    int NoOfIterations = 0;
-        //    if(m_LinearSolver.GetType()== typeof(SoftGMRES))
-        //    ((SoftGMRES)m_LinearSolver).m_SessionPath = m_SessionPath;
-
-        //    OnIterationCallback(NoOfIterations, Solution.CloneAs(), Residual.CloneAs(), this.CurrentLin);
-
-        //    if (CoupledIteration_Converged == null)
-        //        CoupledIteration_Converged = delegate() {
-        //            return true;
-        //        };
-
-        //    // iterate...
-        //    // ==========
-        //    while (( !(ResidualNorm < ConvCrit && CoupledIteration_Converged()) && NoOfIterations < MaxIter) || (NoOfIterations < MinIter)) {
-        //        NoOfIterations++;
-
-        //        //DirectSolver ds = new DirectSolver();
-        //        //ds.Init(this.CurrentLin);
-        //        //double L2_Res = Residual.L2Norm();
-        //        this.m_LinearSolver.Init(this.CurrentLin); 
-        //        Correction.ClearEntries();
-        //        if (Correction.Length != Residual.Length)
-        //            Correction = new double[Residual.Length];
-        //        this.m_LinearSolver.Solve(Correction, Residual);
-
-        //        // Residual may be invalid from now on...
-        //        Solution.AccV(UnderRelax, Correction);
-
-        //        // transform solution back to 'original domain'
-        //        // to perform the linearization at the new point...
-        //        // (and for Level-Set-Updates ...)
-        //        this.CurrentLin.TransformSolFrom(SolutionVec, Solution);
-
-        //        // update linearization
-        //        base.Update(SolutionVec.Mapping.Fields, ref Solution);
-
-        //        // residual evaluation & callback
-        //        base.EvalResidual(Solution, ref Residual);
-        //        ResidualNorm = Residual.L2NormPow2().MPISum().Sqrt();
-        //        OnIterationCallback(NoOfIterations, Solution.CloneAs(), Residual.CloneAs(), this.CurrentLin);
-        //    }
-
-        //}
-
-
     }
-
-
-    /// <summary>
-    /// fix-point iteration with coupled value for convergence (e.g. LevelSet)
-    /// </summary>
-    //public class CoupledFixpointIterator : FixpointIterator {
-
-
-    //    public delegate void DelCoupledIteration(DGField[] locCurSt);
-
-    //    public DelCoupledIteration CoupledIteration;
-
-
-    //    public CoupledFixpointIterator(AssembleMatrixDel __AssembleMatrix, IEnumerable<AggregationGridBasis[]> __AggBasisSeq, 
-    //        MultigridOperator.ChangeOfBasisConfig[][] __MultigridOperatorConfig, DelCoupledIteration __CoupledIter = null) 
-    //        : base(__AssembleMatrix, __AggBasisSeq, __MultigridOperatorConfig) 
-    //    {
-    //        CoupledIteration = __CoupledIter;
-    //    }
-
-
-    //    override public void SolverDriver<S>(CoordinateVector SolutionVec, S RHS) {
-
-    //        // setUp coupled iteration
-    //        // ========================
-
-    //        int NoOfCoupledIter = 0;
-    //        if (CoupledIteration != null) {
-    //            if (CoupledIteration_Converged == null) {
-    //                throw new ArgumentNullException("No Convergence criteria defined for coupled iteration");
-    //            }
-
-    //            NoOfCoupledIter++;
-    //            Console.WriteLine("Coupled Iteration {0}:", NoOfCoupledIter);
-
-    //            this.CoupledIteration(SolutionVec.Mapping.Fields.ToArray());
-
-    //        } else {
-    //            CoupledIteration_Converged = delegate () {
-    //                return true;
-    //            };
-    //        }
-
-
-    //        // initial guess and its residual
-    //        // ==============================
-    //        double[] Solution, Residual;
-    //        base.Init(SolutionVec, RHS, out Solution, out Residual);
-    //        double[] Correction = new double[Solution.Length];
-    //        double ResidualNorm = Residual.L2NormPow2().MPISum().Sqrt();
-    //        int NoOfIterations = 0;
-    //        if (m_LinearSolver.GetType() == typeof(SoftGMRES))
-    //            ((SoftGMRES)m_LinearSolver).m_SessionPath = m_SessionPath;
-
-    //        OnIterationCallback(NoOfIterations, Solution.CloneAs(), Residual.CloneAs(), this.CurrentLin);
-
-
-    //        // iterate...
-    //        // ==========
-    //        //int NoOfMainIterations = 0;
-    //        while ((!(ResidualNorm < ConvCrit && CoupledIteration_Converged()) && NoOfIterations < MaxIter) || (NoOfIterations < MinIter)) {
-    //            NoOfIterations++;
-    //            //NoOfMainIterations++;
-
-    //            //DirectSolver ds = new DirectSolver();
-    //            //ds.Init(this.CurrentLin);
-    //            //double L2_Res = Residual.L2Norm();
-    //            this.m_LinearSolver.Init(this.CurrentLin);
-    //            Correction.ClearEntries();
-    //            if (Correction.Length != Residual.Length)
-    //                Correction = new double[Residual.Length];
-    //            this.m_LinearSolver.Solve(Correction, Residual);
-
-    //            // Residual may be invalid from now on...
-    //            Solution.AccV(UnderRelax, Correction);
-
-    //            // transform solution back to 'original domain'
-    //            // to perform the linearization at the new point...
-    //            // (and for Level-Set-Updates ...)
-    //            this.CurrentLin.TransformSolFrom(SolutionVec, Solution);
-
-
-    //            // coupled iteration
-    //            // ==================
-
-    //            if (CoupledIteration != null && ResidualNorm < ConvCrit && !CoupledIteration_Converged()) {
-    //                NoOfCoupledIter++;
-    //                Console.WriteLine("Coupled Iteration {0}:", NoOfCoupledIter);
-
-    //                this.CoupledIteration(SolutionVec.Mapping.Fields.ToArray());
-
-    //                //NoOfMainIterations = 0;
-    //            }
-
-    //            // ==================
-
-
-    //            // update linearization
-    //            base.Update(SolutionVec.Mapping.Fields, ref Solution);
-
-    //            // residual evaluation & callback
-    //            base.EvalResidual(Solution, ref Residual);
-    //            ResidualNorm = Residual.L2NormPow2().MPISum().Sqrt();
-    //            OnIterationCallback(NoOfIterations, Solution.CloneAs(), Residual.CloneAs(), this.CurrentLin);
-    //        }
-
-    //    }
-
-
-    //    //public override void SolverDriver<S>(CoordinateVector SolutionVec, S RHS) {
-
-    //    //    int iteration = 0;
-
-    //    //    do {
-
-    //    //        iteration++;
-    //    //        Console.WriteLine("Coupled Iteration {0}:", iteration);
-
-    //    //        // coupled iteration step
-    //    //        // =======================
-    //    //        Console.WriteLine("LevelSet-Evolution ...");
-
-    //    //        this.CoupledIteration(SolutionVec.Mapping.Fields.ToArray());
-
-    //    //        if (CoupledIteration_Converged == null)
-    //    //            CoupledIteration_Converged = delegate () {
-    //    //                return true;
-    //    //            };
-
-
-    //    //        // flow solver
-    //    //        // ============
-    //    //        Console.WriteLine("Flow-Solver ...");
-
-
-    //    //        // initial guess and its residual
-    //    //        // ==============================
-    //    //        double[] Solution, Residual;
-    //    //        base.Init(SolutionVec, RHS, out Solution, out Residual);
-    //    //        double[] Correction = new double[Solution.Length];
-    //    //        double ResidualNorm = Residual.L2NormPow2().MPISum().Sqrt();
-    //    //        int NoOfIterations = 0;
-    //    //        if (m_LinearSolver.GetType() == typeof(SoftGMRES))
-    //    //            ((SoftGMRES)m_LinearSolver).m_SessionPath = m_SessionPath;
-
-    //    //        OnIterationCallback(NoOfIterations, Solution.CloneAs(), Residual.CloneAs(), this.CurrentLin);
-
-
-    //    //        // iterate...
-    //    //        // ==========
-    //    //        while ((!(ResidualNorm < ConvCrit) && NoOfIterations < MaxIter) || (NoOfIterations < MinIter)) {
-    //    //            NoOfIterations++;
-
-    //    //            //DirectSolver ds = new DirectSolver();
-    //    //            //ds.Init(this.CurrentLin);
-    //    //            //double L2_Res = Residual.L2Norm();
-    //    //            this.m_LinearSolver.Init(this.CurrentLin);
-    //    //            Correction.ClearEntries();
-    //    //            if (Correction.Length != Residual.Length)
-    //    //                Correction = new double[Residual.Length];
-    //    //            this.m_LinearSolver.Solve(Correction, Residual);
-
-    //    //            // Residual may be invalid from now on...
-    //    //            Solution.AccV(UnderRelax, Correction);
-
-    //    //            // transform solution back to 'original domain'
-    //    //            // to perform the linearization at the new point...
-    //    //            // (and for Level-Set-Updates ...)
-    //    //            this.CurrentLin.TransformSolFrom(SolutionVec, Solution);
-
-    //    //            // update linearization
-    //    //            base.Update(SolutionVec.Mapping.Fields, ref Solution);
-
-    //    //            // residual evaluation & callback
-    //    //            base.EvalResidual(Solution, ref Residual);
-    //    //            ResidualNorm = Residual.L2NormPow2().MPISum().Sqrt();
-    //    //            OnIterationCallback(NoOfIterations, Solution.CloneAs(), Residual.CloneAs(), this.CurrentLin);
-    //    //        }
-
-    //    //    } while (!CoupledIteration_Converged()); 
-
-
-    //    //}
-
-    //}
 
 
 }
