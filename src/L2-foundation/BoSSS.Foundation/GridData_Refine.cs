e3c6fc3928b1e72f10852b95fcba761d8a8b6b00
Martin Smuda <smuda@fdy.tu-darmstadt.de>
2018-01-16 11:28:28 +0100

    intermediate push to exchange, only some refactoring so far

--- src/L2-foundation/BoSSS.Foundation/GridData_Refine.cs
+++ src/L2-foundation/BoSSS.Foundation/GridData_Refine.cs
@@ -44,11 +44,6 @@ namespace BoSSS.Foundation.Grid.Classic {
                 GridCommons oldGrid = this.m_Grid;
                 GridCommons newGrid = new GridCommons(oldGrid.RefElements, oldGrid.EdgeRefElements);
 
-                // Dirty hack for edge tags
-                newGrid.EdgeTagNames.Clear();
-                for (int i = 0; i < oldGrid.EdgeTagNames.Count; i++) {
-                    newGrid.EdgeTagNames.Add(oldGrid.EdgeTagNames.ElementAt(i));
-                }
 
                 Old2New = new GridCorrelation();
                                 
@@ -116,7 +111,7 @@ namespace BoSSS.Foundation.Grid.Classic {
                         CellsToRefineBitmask[jCell] = true;
 
                         int[] Neighs, dummy;
-                        this.GetCellNeighbours(jCell, GetCellNeighbours_Mode.ViaVertices, out Neighs, out dummy);
+                        this.GetCellNeighbours(jCell, GetCellNeighbours_Mode.ViaEdges, out Neighs, out dummy);
 
                         foreach(int jNeigh in Neighs) {
                             AdaptNeighborsBitmask[jNeigh] = true;
@@ -580,7 +575,7 @@ namespace BoSSS.Foundation.Grid.Classic {
                                 for(int i = 0; i < cl.CellFaceTags.Length; i++) {
                                     long ngid = cl.CellFaceTags[i].NeighCell_GlobalID;
                                     if (ngid >= 0)
-                                    Debug.Assert(markers[ngid] == true);
+                                        Debug.Assert(markers[ngid] == true);
                                 }
                             }
                         }
