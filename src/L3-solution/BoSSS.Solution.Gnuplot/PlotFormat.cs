b88616e3db7cbd2c9491f5764da30235eaed724d
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2018-01-29 07:36:35 +0100

    working on 2D-graph plotting (Gnuplot, etc.)

--- src/L3-solution/BoSSS.Solution.Gnuplot/PlotFormat.cs
+++ src/L3-solution/BoSSS.Solution.Gnuplot/PlotFormat.cs
@@ -15,12 +15,96 @@ limitations under the License.
 */
 
 using BoSSS.Platform;
+using Newtonsoft.Json;
+using System;
 using System.Linq;
+using System.Runtime.Serialization;
+
 
 namespace BoSSS.Solution.Gnuplot {
 
+    /// <summary>
+    /// General Style of a plot, i.e. line type, color, etc.
+    /// </summary>
+    [Serializable]
+    [DataContract]
     public class PlotFormat {
+
+        static bool ContainsRemove(ref string f, string token) {
+            token = token.ToLowerInvariant();
+            bool ret = f.Contains(token);
+
+            if(ret)
+                f = f.Replace(token, "");
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Sets the plot from a Matlab-like string, e.g. '--kx' (dashed, black, x-marks).
+        /// </summary>
+        public void FromString(string FormatString) {
+            //linestyle
+            FormatString = FormatString.ToLowerInvariant();
+
+            if (ContainsRemove(ref FormatString, "Black"))
+                this.LineColor = LineColors.Black;
+            if (ContainsRemove(ref FormatString, "Red"))
+                this.LineColor = LineColors.Red;
+            if (ContainsRemove(ref FormatString, "Green"))
+                this.LineColor = LineColors.Green;
+            if (ContainsRemove(ref FormatString, "Blue"))
+                this.LineColor = LineColors.Blue;
+            if (ContainsRemove(ref FormatString, "Yellow"))
+                this.LineColor = LineColors.Yellow;
+            if (ContainsRemove(ref FormatString, "Magenta"))
+                this.LineColor = LineColors.Magenta;
+
+            if (ContainsRemove(ref FormatString, "k"))
+                this.LineColor = LineColors.Black;
+            if (ContainsRemove(ref FormatString, "r"))
+                this.LineColor = LineColors.Red;
+            if (ContainsRemove(ref FormatString, "g"))
+                this.LineColor = LineColors.Green;
+            if (ContainsRemove(ref FormatString, "b"))
+                this.LineColor = LineColors.Blue;
+            if (ContainsRemove(ref FormatString, "y"))
+                this.LineColor = LineColors.Yellow;
+            if (ContainsRemove(ref FormatString, "m"))
+                this.LineColor = LineColors.Magenta;
+
+
+
+            if (ContainsRemove(ref FormatString, "--"))
+                this.DashType = DashTypes.Dashed;
+            if (ContainsRemove(ref FormatString, ":"))
+                this.DashType = DashTypes.Dotted;
+            if (ContainsRemove(ref FormatString, "-."))
+                this.DashType = DashTypes.DotDashed;
+            if (ContainsRemove(ref FormatString, "-"))
+                this.DashType = DashTypes.Solid;
+
+            if (ContainsRemove(ref FormatString, "+"))
+                this.PointType = PointTypes.Plus;
+            if (ContainsRemove(ref FormatString, "o"))
+                this.PointType = PointTypes.OpenCircle;
+            if (ContainsRemove(ref FormatString, "*"))
+                this.PointType = PointTypes.Asterisk;
+            if (ContainsRemove(ref FormatString, "x"))
+                this.PointType = PointTypes.Times;
+            if (ContainsRemove(ref FormatString, "s"))
+                this.PointType = PointTypes.OpenBox;
+            if (ContainsRemove(ref FormatString, "^"))
+                this.PointType = PointTypes.OpenLowerTriangle;
+            if (ContainsRemove(ref FormatString, "^"))
+                this.PointType = PointTypes.LowerTriangle;
+
+        }
+
                
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public PlotFormat(PlotFormat baseLineFormat = null, LineColors? lineColor = null, DashTypes? dashType = null, double? lineWidth = null, Styles? Style = null, PointTypes? pointType = null, double? pointSize = null) {
             if (baseLineFormat != null) {
                 this.lineColor = lineColor ?? baseLineFormat.lineColor;
@@ -39,58 +123,113 @@ namespace BoSSS.Solution.Gnuplot {
             }
         }
 
-        private readonly LineColors? lineColor;
+        [DataMember]
+        private LineColors? lineColor;
 
+        /// <summary>
+        /// Color
+        /// </summary>
+        [JsonIgnore]
         public LineColors LineColor {
             get {
                 return lineColor ?? LineColors.Red;
             }
+            set {
+                lineColor = value;
+            }
         }
 
-        private readonly DashTypes? dashType;
+        [DataMember]
+        private DashTypes? dashType;
 
+        /// <summary>
+        /// In a plot which uses lines, the style of those.
+        /// </summary>
+        [JsonIgnore]
         public DashTypes DashType {
             get {
                 return dashType ?? DashTypes.Solid;
             }
+            set {
+                dashType = value;
+            }
         }
 
-        private readonly double? lineWidth;
+        [DataMember]
+        private double? lineWidth;
 
+        /// <summary>
+        /// line width
+        /// </summary>
+        [JsonIgnore]
         public double LineWidth {
             get {
                 return lineWidth ?? 1.0;
             }
+            set {
+                lineWidth = value;
+            }
         }
 
-        private readonly Styles? _Style;
+        [DataMember]
+        private Styles? _Style;
 
+        /// <summary>
+        /// Basic plot type, e.g. lines, points, bars, etc.
+        /// </summary>
+        [JsonIgnore]
         public Styles Style {
             get {
                 return _Style ?? Styles.Lines;
             }
+            set {
+                _Style = value;
+            }
         }
 
-        private readonly PointTypes? pointType;
+        [DataMember]
+        private PointTypes? pointType;
 
+        /// <summary>
+        /// Point type, only effective for <see cref="Styles.Points"/> and <see cref="Styles.LinesPoints"/>.
+        /// </summary>
+        [JsonIgnore]
         public PointTypes PointType {
             get {
                 return pointType ?? PointTypes.OpenCircle; // dass man zumindest irgendwas sieht, wenn man auf 'LinesPoints' stellt
             }
+            set {
+                pointType = value;
+            }
         }
 
-        private readonly double? pointSize;
+        [DataMember]
+        private double? pointSize;
 
+        /// <summary>
+        /// Size, may depend on output driver (screen or LaTeX),
+        /// only effective for <see cref="Styles.Points"/> and <see cref="Styles.LinesPoints"/>.
+        /// </summary>
+        [JsonIgnore]
         public double PointSize {
             get {
                 return pointSize ?? 1.0;
             }
+            set {
+                pointSize = value;
+            }
         }
 
+        /// <summary>
+        /// Cloning and setting <see cref="LineColor"/>.
+        /// </summary>
         public PlotFormat WithLineColor(LineColors color) {
             return new PlotFormat(baseLineFormat: this, lineColor: color);
         }
 
+        /// <summary>
+        /// Cloning and setting <see cref="LineColor"/>.
+        /// </summary>
         public PlotFormat WithLineColor(int i) {
             int numberOfLineColors = Enum<LineColors>.GetValues().Count();
             return new PlotFormat(
@@ -98,14 +237,23 @@ namespace BoSSS.Solution.Gnuplot {
                 lineColor: (LineColors)((i + 1) % numberOfLineColors));
         }
 
+        /// <summary>
+        /// Cloning and setting <see cref="LineWidth"/>.
+        /// </summary>
         public PlotFormat WithLineWidth(double lineWidth) {
             return new PlotFormat(baseLineFormat: this, lineWidth: LineWidth);
         }
 
+        /// <summary>
+        /// Cloning and setting <see cref="DashType"/>.
+        /// </summary>
         public PlotFormat WithDashType(DashTypes dashtype) {
             return new PlotFormat(baseLineFormat: this, dashType: dashtype);
         }
 
+        /// <summary>
+        /// Cloning and setting <see cref="DashType"/>.
+        /// </summary>
         public PlotFormat WithDashType(int i) {
             int numberOfDashTypes = Enum<DashTypes>.GetValues().Count();
             return new PlotFormat(
@@ -113,14 +261,23 @@ namespace BoSSS.Solution.Gnuplot {
                 dashType: (DashTypes)((i + 1) % numberOfDashTypes));
         }
 
+        /// <summary>
+        /// Cloning and setting <see cref="PointSize"/>.
+        /// </summary>
         public PlotFormat WithPointSize(double pointSize) {
             return new PlotFormat(baseLineFormat: this, pointSize: pointSize);
         }
 
+        /// <summary>
+        /// Cloning and setting <see cref="pointType"/>.
+        /// </summary>
         public PlotFormat WithPointType(PointTypes pointType) {
             return new PlotFormat(baseLineFormat: this, pointType: pointType);
         }
 
+        /// <summary>
+        /// Cloning and setting <see cref="pointType"/>.
+        /// </summary>
         public PlotFormat WithPointType(int i) {
             int numberOfPointTypes = Enum<PointTypes>.GetValues().Count();
             return new PlotFormat(
@@ -128,6 +285,9 @@ namespace BoSSS.Solution.Gnuplot {
                 pointType: (PointTypes)(i % numberOfPointTypes));
         }
 
+        /// <summary>
+        /// Cloning and setting <see cref="Style"/>.
+        /// </summary>
         public PlotFormat WithStyle(Styles style) {
             return new PlotFormat(baseLineFormat: this, Style: style);
         }
