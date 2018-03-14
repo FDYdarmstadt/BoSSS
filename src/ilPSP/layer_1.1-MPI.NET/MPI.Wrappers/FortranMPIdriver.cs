43cccb38181c18a8b7525a5925a3f2a7c5645827
florian Kummer <kummer@fdy.tu-darmstadt.de>
2018-03-14 17:44:54 +0100

    Recovery: what have I done?

--- src/ilPSP/layer_1.1-MPI.NET/MPI.Wrappers/FortranMPIdriver.cs
+++ src/ilPSP/layer_1.1-MPI.NET/MPI.Wrappers/FortranMPIdriver.cs
@@ -393,6 +393,7 @@ namespace MPI.Wrappers {
         /// <param name="index"></param>
         /// <param name="status"></param>
         public void Waitany(int count, MPI_Request[] array_of_requests, out int index, out MPI_Status status) {
+            using(FuncTrace())
             int ierr;
 
             // note: since the status is passed by reference/pointer, 
