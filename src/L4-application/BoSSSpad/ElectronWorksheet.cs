9139a92c2a6185122dae08ba155d26ad865956b4
Lauritz Beck <lauritzbeck@gmail.com>
2018-04-23 18:58:53 +0200

    Node/Electron BoSSSPad GUI. WIP. See public/src/BoSSSpadGUI/README.txt for installing details.

---
+++ src/L4-application/BoSSSpad/ElectronWorksheet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using System.Globalization;
+using BoSSS.Foundation.IO;
+
+namespace BoSSS.Application.BoSSSpad{
+
+    public class ElectronWorksheet{
+        Document document;
+
+        public ElectronWorksheet(){
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            // launch the app
+            // ==============
+            ilPSP.Environment.Bootstrap(
+                new string[0],
+                Utils.GetBoSSSInstallDir(),
+                out bool mpiInitialized
+            );
+            this.document = new Document();
+        }
+
+        public string RunCommand(string command){
+
+            Document.Tuple singleCommandAndResult = new Document.Tuple{
+                Command = command
+            };
+
+            singleCommandAndResult.Evaluate();
+            return singleCommandAndResult.InterpreterTextOutput;
+        }
+
+        //save
+
+        //load
+    }
+}
