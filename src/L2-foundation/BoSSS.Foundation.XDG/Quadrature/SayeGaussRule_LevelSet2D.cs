f916b0e51e022870c3bfa0c62d68d47091ce903c
Lauritz Beck <lauritzbeck@gmail.com>
2018-05-02 17:16:49 +0200

    Created files for SayeGauss Quadrature Rule.

---
+++ src/L2-foundation/BoSSS.Foundation.XDG/Quadrature/SayeGaussRule_LevelSet2D.cs
@@ -0,0 +1,36 @@
+﻿using BoSSS.Foundation.Quadrature;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoSSS.Foundation.Grid;
+using BoSSS.Foundation.Grid.RefElements;
+
+namespace BoSSS.Foundation.XDG.Quadrature
+{
+
+    /// <summary>
+    /// Gauss rules for \f$ \oint_{\frakI \cap K_j } \ldots \dS \f$ in the 2D case
+    /// </summary>
+    class SayeGaussRule_LevelSet2D : IQuadRuleFactory<QuadRule>
+    {
+        public RefElement RefElement {
+            get {
+                return Square.Instance;
+            }
+        }
+
+        public int[] GetCachedRuleOrders()
+        {
+            return null;
+        }
+
+        public IEnumerable<IChunkRulePair<QuadRule>> GetQuadRuleSet(ExecutionMask mask, int order)
+        {
+            Line.Instance.GetQuadratureRule()
+
+            throw new NotImplementedException();
+        }
+    }
+}
