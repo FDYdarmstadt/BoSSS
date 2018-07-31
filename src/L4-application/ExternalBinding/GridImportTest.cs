ad228e7c8d6789af05f4d620d7eacf2678c288ba
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2018-07-31 15:58:49 +0200

    foam-binding: conversion of FOAM mesh to BoSSS, creating unsafe wrapper/driver

--- src/L4-application/ExternalBinding/GridImportTest.cs
+++ src/L4-application/ExternalBinding/GridImportTest.cs
@@ -3,6 +3,7 @@ using BoSSS.Foundation.Grid;
 using BoSSS.Foundation.Grid.Classic;
 using BoSSS.Foundation.Grid.RefElements;
 using ilPSP;
+using ilPSP.Utils;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -15,260 +16,13 @@ namespace BoSSS.Application.ExternalBinding {
 
         public static void Main() {
 
-            // Checks
-            // ======
-
             Common_.BoSSSInitialize();
-            Debug.Assert(nFaces == faces.Length);
-            Debug.Assert(nInternalFaces == neighbour.Length);
-            Debug.Assert(nFaces == owner.Length);
-            Debug.Assert(nPoints == points.Length);
 
-
-            // Build Cells-to-Faces correlation
-            // ================================
-
-            List<int>[] Cells2Faces = new List<int>[nCells];
-            for(int j = 0; j < nCells; j++) {
-                Cells2Faces[j] = new List<int>();
-            }
-
-            for(int iFace = 0; iFace < nFaces; iFace++) {
-                Debug.Assert(Cells2Faces[owner[iFace]].Contains(iFace) == false);
-                Cells2Faces[owner[iFace]].Add(iFace);
-
-            }
-
-            for(int iInternalFace = 0; iInternalFace < nInternalFaces; iInternalFace++) {
-                int NeighCell = neighbour[iInternalFace];
-
-                Debug.Assert(Cells2Faces[NeighCell].Contains(iInternalFace) == false);
-                Cells2Faces[NeighCell].Add(iInternalFace);
-            }
-
-            // convert to BoSSS grid
-            // =====================
-
-            Cell[] bosss_cells = new Cell[nCells];
-
-            for(int iCell = 0; iCell < nCells; iCell++) {
-                var Cell2Faces = Cells2Faces[iCell];
-
-                if(    Cell2Faces.Count == 6
-                   && !Cell2Faces.Select(iFace => faces[iFace].Length == 4).Contains(false)) {
-                    // +++++++++++++++
-                    // found some Cube
-                    // +++++++++++++++
-
-                    int[][] cube_faces = Cell2Faces.Select(iFace => faces[iFace]).ToArray();
-                    Debug.Assert(cube_faces.Length == 6);
-
-                    HashSet<int> NodesUnsorted = new HashSet<int>();
-                    foreach(int iFace in Cell2Faces) {
-                        foreach(int iPoint in faces[iFace]) {
-                            NodesUnsorted.Add(iPoint);
-                        }
-                    }
-                    if(NodesUnsorted.Count != 8) {
-                        throw new NotImplementedException("Degenerate cubes are not supported.");
-                    }
-
-                    // create cell
-                    Cell cl = new Cell();
-                    bosss_cells[iCell] = cl;
-                    cl.GlobalID = iCell;
-                    cl.Type = CellType.Cube_8;
-                    cl.NodeIndices = new int[8];
-                    cl.TransformationParams = MultidimensionalArray.Create(8, 3);
-
-                    // Find point indices for bottom, i.e. 'y == -1' (in ref. coordinates):
-                    cl.NodeIndices[0] = cube_faces[0][0];
-                    cl.NodeIndices[1] = cube_faces[0][1];
-                    cl.NodeIndices[6] = cube_faces[0][2];
-                    cl.NodeIndices[3] = cube_faces[0][3];
-
-
-                    // Find point indices for 'x == -1'-wall (in ref. coordinates):
-                    bool found = false;
-                    for(int iCF = 1; iCF < 6; iCF++) {
-                        found = ContainsEdge(cube_faces[iCF], cl.NodeIndices[0], cl.NodeIndices[3], out cl.NodeIndices[2], out cl.NodeIndices[7]);
-                        if (found)
-                            break;
-                    }
-                    if (found == false)
-                        throw new NotSupportedException("weired cell topology in cell " + iCell + "; assuming a cube/hex;");
-
-                    // Find point indices for 'x == +1'-wall (in ref. coordinates):
-                    found = false;
-                    for(int iCF = 1; iCF < 6; iCF++) {
-                        found = ContainsEdge(cube_faces[iCF], cl.NodeIndices[1], cl.NodeIndices[6], out cl.NodeIndices[4], out cl.NodeIndices[5]);
-                        if (found)
-                            break;
-                    }
-                    if (found == false)
-                        throw new NotSupportedException("weired cell topology in cell " + iCell + "; assuming a cube/hex;");
-
-
-                    // set point coordinates
-                    for(int i = 0; i < 8; i++) {
-                        cl.TransformationParams.SetRow(i, points[cl.NodeIndices[i]]);
-                    }
-
-                    // Jacobian Test
-                    JacobianTest(cl, out bool PositiveJacobianFlag, out bool NegativeJacobianFlag, out bool Linear);
-                    if(NegativeJacobianFlag) {
-                        // flip top/bottom
-                        int _0 = cl.NodeIndices[0];
-                        int _1 = cl.NodeIndices[1];
-                        int _6 = cl.NodeIndices[6];
-                        int _3 = cl.NodeIndices[3];
-
-                        cl.NodeIndices[0] = cl.NodeIndices[2];
-                        cl.NodeIndices[1] = cl.NodeIndices[4];
-                        cl.NodeIndices[6] = cl.NodeIndices[5];
-                        cl.NodeIndices[3] = cl.NodeIndices[7];
-
-                        cl.NodeIndices[2] = _0;
-                        cl.NodeIndices[4] = _1;
-                        cl.NodeIndices[5] = _6;
-                        cl.NodeIndices[7] = _3;
-                        
-                        for (int i = 0; i < 8; i++) {
-                            cl.TransformationParams.SetRow(i, points[cl.NodeIndices[i]]);
-                        }
-
-                        JacobianTest(cl, out PositiveJacobianFlag, out NegativeJacobianFlag, out Linear);
-                    }
-
-                    if(NegativeJacobianFlag || !PositiveJacobianFlag) {
-                        throw new NotSupportedException("Found degenerate Jacobian in cell " + iCell);
-                    }
-                    Debug.Assert(PositiveJacobianFlag == true);
-                    Debug.Assert(NegativeJacobianFlag == false);
-
-                    if (Linear) {
-                        cl.Type = CellType.Cube_Linear;
-                        cl.TransformationParams = cl.TransformationParams.ExtractSubArrayShallow(new int[] { 0, 0 }, new int[] { 3, 2 }).CloneAs();
-                    }
-                    
-
-                } else {
-                    throw new NotImplementedException("At the moment, only support for hex cells.");
-                }
-            }
-
-            // final test
-            // ==========
-
-            GridCommons grd = new GridCommons(new RefElement[] { Cube.Instance }, new RefElement[] { Square.Instance }) {
-                Cells = bosss_cells,
-                Description = "imported form OpenFOAM"
-            };
-
-            var gDat = new GridData(grd);
+            int nCells = 9;
 
 
 
-            // Finalize
-            // ========
-            Common_.BoSSSFinalize();
-        }
-
-
-        static void JacobianTest(Cell cl, out bool PositiveJacobianFlag, out bool NegativeJacobianFlag, out bool Linear) {
-            RefElement Kref = cl.Type.GetRefElement();
-
-            int D = 3; // for OpenFOAM, we assume 3D;
-
-            // get nodes within ref element, to test the Jacobian
-            int deg = Kref.GetInterpolationDegree(cl.Type);
-            if (deg > 1)
-                deg--;
-            deg *= 3;
-
-            NodeSet TestNodes = Kref.GetQuadratureRule(2 * deg).Nodes;
-
-            // evaluate derivatives of nodal polynomials for transformation
-            PolynomialList[] Deriv = Kref.GetInterpolationPolynomials1stDeriv(cl.Type);
-            MultidimensionalArray[] DerivEval = new MultidimensionalArray[D];
-            for (int d = 0; d < D; d++) {
-                DerivEval[d] = Deriv[d].Values.GetValues(TestNodes);
-            }
-
-            // evaluate Jacobian matrix
-            MultidimensionalArray Jacobi = MultidimensionalArray.Create(TestNodes.NoOfNodes, D, D); // temporary storage for Jacobian matrix
-
-            Debug.Assert(cl.TransformationParams.Dimension == 2);
-            Debug.Assert(cl.TransformationParams.GetLength(1) == 3);
-
-            for (int d1 = 0; d1 < D; d1++) {
-                Debug.Assert(cl.TransformationParams.GetLength(0) == Deriv[d1].Count);
-                MultidimensionalArray JacobiCol = Jacobi.ExtractSubArrayShallow(-1, -1, d1);
-                JacobiCol.Multiply(1.0, DerivEval[d1], cl.TransformationParams, 0.0, "kd", "kn", "nd");
-            }
-
-            // do the tests
-            NegativeJacobianFlag = false;
-            PositiveJacobianFlag = false;
-            double MinAbsJacobiDet = double.MaxValue;
-            double MaxAbsJacobiDet = 0.0;
-            for (int n = 0; n < TestNodes.NoOfNodes; n++) {
-                double detJac = Jacobi.ExtractSubArrayShallow(n, -1, -1).Determinant();
-                if (detJac <= 0) {
-                    NegativeJacobianFlag = true;
-                }
-                if (detJac > 0) {
-                    PositiveJacobianFlag = true;
-                }
-
-                MinAbsJacobiDet = Math.Min(MinAbsJacobiDet, detJac.Abs());
-                MaxAbsJacobiDet = Math.Max(MaxAbsJacobiDet, detJac.Abs());
-            }
-
-            if ((MaxAbsJacobiDet - MinAbsJacobiDet) / MinAbsJacobiDet <= 1.0e-8)
-                Linear = true;
-            else
-                Linear = false;
-
-        }
-
-
-        static bool ContainsEdge(int[] face, int iPt1, int iPt2, out int iPrev, out int iNext) {
-            int L = face.Length;
-            for (int i = 0; i < L; i++) {
-                int v1 = face[i];
-                int v2 = face[(i + 1) % L];
-                int _iPrev = face[i > 0 ? (i - 1) : (L - 1)];
-                int _iNext = face[(i + 2) % L];
-
-                if (v1 == iPt1 && v2 == iPt2) {
-                    iPrev = _iPrev;
-                    iNext = _iNext;
-                    return true;
-                }
-                if (v2 == iPt1 && v1 == iPt2) {
-                    iNext = _iPrev;
-                    iPrev= _iNext;
-                    return true;
-                }
-            }
-            iNext = int.MinValue;
-            iPrev = int.MinValue;
-            return false;
-        }
-
-
-        // ---------------------------------
-        // test data from OpenFOAM tutorials
-        //
-
-        static int nPoints = 32;
-        static int nCells = 9;
-        static int nFaces = 42;
-        static int nInternalFaces = 12;
-
-
-        static int[][] faces = new int[][] {
+            int[][] faces = new int[][] {
             new int[] {1, 5, 21, 17},
             new int[] {4, 20, 21, 5},
             new int[] {2, 6, 22, 18},
@@ -313,7 +67,7 @@ namespace BoSSS.Application.ExternalBinding {
             new int[] {26, 27, 31, 30}
         };
 
-        static int[] neighbour = new int[] {
+            int[] neighbour = new int[] {
             1,
             3,
             2,
@@ -328,7 +82,7 @@ namespace BoSSS.Application.ExternalBinding {
             8
         };
 
-        static int[] owner = new int[] {
+            int[] owner = new int[] {
             0,
             0,
             1,
@@ -373,7 +127,7 @@ namespace BoSSS.Application.ExternalBinding {
             8
         };
 
-        static double[][] points = new double[][] {
+            double[][] points = new double[][] {
             new double[] {0,0,0},
             new double[] {0.03333333333,0,0},
             new double[] {0.06666666667,0,0},
@@ -407,5 +161,20 @@ namespace BoSSS.Application.ExternalBinding {
             new double[] {0.06666666667,0.1,0.01},
             new double[] {0.1,0.1,0.01}
         };
+
+
+
+
+            Common_.BoSSSFinalize();
+        }
+
+
+        
+
+        // ---------------------------------
+        // test data from OpenFOAM tutorials
+        //
+
+        
     }
 }
