3a2a497e02efd2181950f21af5ad6eaad2a30ba4
Markus Geisenhofer <geisenhofer@fdy.tu-darmstadt.de>
2018-11-26 09:36:22 +0100

    CNS IBM Piston tests: Set threshold of MovingMeshIBMPiston1stOrderWithAgglomeration

--- src/L4-application/CNS/Tests/MovingIBMTests/PistonTests.cs
+++ src/L4-application/CNS/Tests/MovingIBMTests/PistonTests.cs
@@ -239,7 +239,7 @@ namespace CNS.Tests.MovingIBMTests {
                 solver.QueryHandler.QueryResults,
                 Tuple.Create("L2ErrorDensity", 2.4e-4),
                 Tuple.Create("L2ErrorXMomentum", 2.0e-4),
-                Tuple.Create("L2ErrorYMomentum", 7.8e-6),
+                Tuple.Create("L2ErrorYMomentum", 9.0e-6),
                 Tuple.Create("L2ErrorPressure", 2.8e-4));
         }
 
