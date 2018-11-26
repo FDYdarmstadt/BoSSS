1b6924dde852cfcfa4361f091910c9a0460b7675
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2018-11-26 08:29:58 +0100

    Voronoi: implemented triangulation of non-convex polygons

--- src/L4-application/ipPoisson/PolygonTesselation.cs
+++ src/L4-application/ipPoisson/PolygonTesselation.cs
@@ -14,10 +14,19 @@ namespace BoSSS.Application.SipPoisson {
     static class PolygonTesselation {
 
         static double sign(Vector p1, Vector p2, Vector p3) {
+            if (p1.Dim != 2)
+                throw new ArgumentException();
+            if (p2.Dim != 2)
+                throw new ArgumentException();
+            if (p3.Dim != 2)
+                throw new ArgumentException();
+
             return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
         }
 
         static bool PointInTriangle(Vector pt, Vector v1, Vector v2, Vector v3) {
+
+
             double d1, d2, d3;
             bool has_neg, has_pos;
 
@@ -30,5 +39,31 @@ namespace BoSSS.Application.SipPoisson {
 
             return !(has_neg && has_pos);
         }
+
+
+        static bool IsEar(List<Vector> Polygon, int iTst) {
+            int L = Polygon.Count;
+
+            if (iTst < 0)
+                throw new IndexOutOfRangeException();
+            if (iTst >= L)
+                throw new IndexOutOfRangeException();
+
+            int iNxt = iTst + 1;
+            if (iNxt >= L)
+                iNxt -= L;
+            int iPrv = iTst - 1;
+            if (iPrv < 0)
+                iPrv += L;
+
+            Vector vPrv = Polygon[iPrv];
+            Vector vTst = Polygon[iTst];
+            Vector vNxt = Polygon[iNxt];
+
+        }
+    
+
+
+
     }
 }
