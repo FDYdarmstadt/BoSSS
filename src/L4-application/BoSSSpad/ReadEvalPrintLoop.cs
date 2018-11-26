a45c27f5bbed2a0a8d155a63d920c8ca6eb8d950
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2018-11-26 15:34:14 +0100

    added rheology solver to BoSSSpad

--- src/L4-application/BoSSSpad/ReadEvalPrintLoop.cs
+++ src/L4-application/BoSSSpad/ReadEvalPrintLoop.cs
@@ -117,8 +117,7 @@ namespace BoSSS.Application.BoSSSpad {
             eval.ReferenceAssembly(typeof(IBM_Solver.IBM_SolverMain).Assembly);
             eval.ReferenceAssembly(typeof(XNSE_Solver.XNSE_SolverMain).Assembly);
             eval.ReferenceAssembly(typeof(BoSSS.Application.SipPoisson.SipPoissonMain).Assembly);
-            
-            //eval.ReferenceAssembly(typeof(Rheology.Rheology).Assembly);
+            eval.ReferenceAssembly(typeof(Rheology.Rheology).Assembly);
             
 
             //eval.ReferenceAssembly(typeof(FuelCell.FuelCellMain).Assembly);
