2d31a64b380a70213f2e44f815625cd8316332d0
florian Kummer <kummer@fdy.tu-darmstadt.de>
2018-12-06 17:11:03 +0100

    Voronoi, clipping: testcode

--- src/L4-application/ipPoisson/SipHardcodedControl.cs
+++ src/L4-application/ipPoisson/SipHardcodedControl.cs
@@ -698,7 +698,8 @@ namespace BoSSS.Application.SipPoisson {
                 bool t2 = (A - B).AbsSquare() < 1.0e-10;
                 bool t1 = (A - B).AbsSquare() < 1.0e-15;
                 //Debug.Assert(t1 == t2);
-                return t1;
+                Voronoi.VoronoiMeshGen.AccuracyFlag = (t1 == t2);
+                return t2;
             }
             
             IGrid GridFunc() {
