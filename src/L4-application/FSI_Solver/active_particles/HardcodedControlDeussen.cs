8a581cb6dc4369845a19ba1084abb442064f3dc6
Benjamin Deußen <deussen@fdy.tu-darmstadt.de>
2018-12-07 12:21:37 +0100

    FSI underrelaxation complete

--- src/L4-application/FSI_Solver/active_particles/HardcodedControlDeussen.cs
+++ src/L4-application/FSI_Solver/active_particles/HardcodedControlDeussen.cs
@@ -254,7 +254,7 @@ namespace BoSSS.Application.FSI_Solver
             C.MaxSolverIterations = 1000;
             C.MinSolverIterations = 1;
             C.NoOfMultigridLevels = 1;
-            C.LevelSet_ConvergenceCriterion = 1e-6;
+            C.LevelSet_ConvergenceCriterion = 5e-9;
             C.LSunderrelax = 1.0;
 
 
