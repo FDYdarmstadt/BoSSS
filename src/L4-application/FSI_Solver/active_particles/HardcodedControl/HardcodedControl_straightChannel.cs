300c3a5cbf5912e10536d051bcd490dbf1b0ed39
Benjamin Deußen <deussen@fdy.tu-darmstadt.de>
2019-03-22 10:22:32 +0100

    FSI fxd bug which caused F[1] to be zero

--- src/L4-application/FSI_Solver/active_particles/HardcodedControl/HardcodedControl_straightChannel.cs
+++ src/L4-application/FSI_Solver/active_particles/HardcodedControl/HardcodedControl_straightChannel.cs
@@ -65,8 +65,8 @@ namespace BoSSS.Application.FSI_Solver
                 int q = new int(); // #Cells in x-dircetion + 1
                 int r = new int(); // #Cells in y-dircetion + 1
 
-                q = 75/1;
-                r = 40/1;
+                q = 75/2;
+                r = 40/2;
 
                 double[] Xnodes = GenericBlas.Linspace(-7.5 * BaseSize, 7.5 * BaseSize, q);
                 double[] Ynodes = GenericBlas.Linspace(-4 * BaseSize, 4 * BaseSize, r);
@@ -130,7 +130,7 @@ namespace BoSSS.Application.FSI_Solver
             int numOfParticles = 1;
             for (int d = 0; d < numOfParticles; d++)
             {
-                C.Particles.Add(new Particle_Ellipsoid(new double[] { 0 , 0.0 }, startAngl: 0)
+                C.Particles.Add(new Particle_Ellipsoid(new double[] { 0 , 0.0 }, startAngl: 45)
                 {
                     particleDensity = 0,
                     ActiveParticle = true,
@@ -138,7 +138,7 @@ namespace BoSSS.Application.FSI_Solver
                     thickness_P = 0.2 * BaseSize,
                     length_P = 1 * BaseSize,
                     AddaptiveUnderrelaxation = true,// set true if you want to define a constant underrelaxation (not recommended)
-                    underrelaxation_factor = 5,// underrelaxation with [factor * 10^exponent]
+                    underrelaxation_factor = 0.1,// underrelaxation with [factor * 10^exponent]
                     ClearSmallValues = true,
                     neglectAddedDamping = false
             });
@@ -191,7 +191,7 @@ namespace BoSSS.Application.FSI_Solver
             C.LinearSolver.NoOfMultigridLevels = 1;
             C.LinearSolver.MaxSolverIterations = 1000;
             C.LinearSolver.MinSolverIterations = 1;
-            C.ForceAndTorque_ConvergenceCriterion = 2e1;
+            C.ForceAndTorque_ConvergenceCriterion = 10;
             C.LSunderrelax = 1.0;
             
 
