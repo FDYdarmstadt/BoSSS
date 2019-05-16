fa43574fbf1381f265e030fc3981ece1450b99a0
Markus Geisenhofer <geisenhofer@fdy.tu-darmstadt.de>
2019-05-16 14:17:03 +0200

    CNS refactoring: Moved parts of CNSControl (L4) to compressible control (L3) and created XDGEulerControl

--- src/L4-application/CNS/CNSControl.cs
+++ src/L4-application/CNS/CNSControl.cs
@@ -128,40 +128,6 @@ namespace CNS {
         }
 
         /// <summary>
-        /// Utility function to add a new variable to the solver.
-        /// </summary>
-        /// <param name="variable">
-        /// The variable to be added
-        /// </param>
-        /// <param name="degree">
-        /// The desired polynomial degree of the variable
-        /// </param>
-        /// <param name="saveToDB">
-        /// Bool indicating whether the given variable shall be saved to the
-        /// database in each saved time-step
-        /// </param>
-        public void AddVariable(Variable variable, int degree, bool saveToDB = true) {
-            variableFields.Add(variable, degree);
-
-            FieldOpts.SaveToDBOpt option;
-            if (saveToDB) {
-                option = FieldOpts.SaveToDBOpt.TRUE;
-            } else {
-                option = FieldOpts.SaveToDBOpt.FALSE;
-            }
-
-            FieldOptions.Add(variable, new FieldOpts() {
-                Degree = degree,
-                SaveToDB = option
-            });
-        }
-
-        /// <summary>
-        /// Backing field for <see cref="VariableFields"/>
-        /// </summary>
-        private Dictionary<Variable, int> variableFields = new Dictionary<Variable, int>();
-
-        /// <summary>
         /// Dictionary linking field variables (including derived ones) to
         /// the desired polynomial degree
         /// </summary>
@@ -248,53 +214,10 @@ namespace CNS {
         public bool FluxCorrection = true;
 
         /// <summary>
-        /// The configured Reynolds number in the far field.
-        /// </summary>
-        /// <remarks>
-        /// This option is ignored if <see cref="DomainType"/> is equal
-        /// to "Euler"
-        /// </remarks>
-        public double ReynoldsNumber;
-
-        /// <summary>
-        /// The configured Prandtl number in the far field.
-        /// </summary>
-        /// <remarks>
-        /// This option is ignored if <see cref="DomainType"/> is equal
-        /// to "Euler"
-        /// </remarks>
-        public double PrandtlNumber;
-
-        /// <summary>
-        /// The ratio of a characteristic flow velocity to the velocity of a
-        /// gravitational wave.
-        /// </summary>
-        [InclusiveLowerBound(0.0)]
-        public double FroudeNumber;
-
-        /// <summary>
-        /// The ratio of the bulk viscosity to the shear viscosity.
-        /// </summary>
-        [InclusiveLowerBound(0.0)]
-        public double ViscosityRatio = 0.0;
-
-        /// <summary>
-        /// The viscosity law to be used, i.e. the variation of the viscosity
-        /// due to temperature changes.
-        /// </summary>
-        public IViscosityLaw ViscosityLaw = new ConstantViscosity();
-
-        /// <summary>
         /// Required for the SIPG method. Should be > 1
         /// </summary>
         [ExclusiveLowerBound(0.0)]
         public double SIPGPenaltyScaling = 1.0;
-
-        /// <summary>
-        /// Material parameters to be used
-        /// </summary>
-        [NotNull]
-        public IEquationOfState EquationOfState = IdealGas.Air;
 
         /// <summary>
         /// The type of residual logger to be used, see
@@ -308,14 +231,6 @@ namespace CNS {
         /// </summary>
         [InclusiveLowerBound(0)]
         public int ResidualInterval = 1;
-
-        /// <summary>
-        /// If set to a positive value, defines the interval (in terms of the
-        /// time-step number) between log messages on the console (e.g., to
-        /// keep file sizes smaller for long runs)
-        /// </summary>
-        [InclusiveLowerBound(0.0)]
-        public int PrintInterval = 1;
 
         /// <summary>
         /// A mapping between residual variables and the corresponding
@@ -429,13 +344,5 @@ namespace CNS {
         public override Type GetSolverType() {
             return typeof(CNS.Program);
         }
-
-        /// <summary>
-        /// %
-        /// </summary>
-        virtual public Material GetMaterial() {
-            return new Material(EquationOfState, ViscosityLaw, MachNumber, ReynoldsNumber, PrandtlNumber, FroudeNumber, ViscosityRatio);
-        }
-
     }
 }
--- src/L4-application/CNS/TestCases.cs
+++ src/L4-application/CNS/TestCases.cs
@@ -38,13 +38,13 @@ namespace CNS {
 
     public static class TestCases {
 
-        public static CNSControl ShockTube(string dbPath = null, int savePeriod = 100, int dgDegree = 2, int numOfCellsX = 50, int numOfCellsY = 50, double sensorLimit = 1e-3, double dtFixed = 0.0, double CFLFraction = 0.1, int explicitScheme = 3, int explicitOrder = 1, int numberOfSubGrids = 3, int reclusteringInterval = 1, int maxNumOfSubSteps = 0, int refinementLevel = 0) {
+        public static CNSControl ShockTube(string dbPath = null, int savePeriod = 100, int dgDegree = 2, int numOfCellsX = 50, int numOfCellsY = 50, double sensorLimit = 1e-3, double dtFixed = 0.0, double CFLFraction = 0.1, int explicitScheme = 1, int explicitOrder = 1, int numberOfSubGrids = 3, int reclusteringInterval = 1, int maxNumOfSubSteps = 0, int refinementLevel = 0) {
             CNSControl c = new CNSControl();
 
             // ### Database ###
             //dbPath = @"/work/scratch/ws35kire/work_db";                       // Lichtenberg
             //dbPath = @"/home/ws35kire/test_db";                               // Lichtenberg
-            dbPath = @"c:\bosss_db";                                          // Local
+            //dbPath = @"c:\bosss_db";                                          // Local
             //dbPath = @"\\dc1\userspace\geisenhofer\bosss_db_IBMShockTube";    // Network
 
             c.DbPath = dbPath;
@@ -52,7 +52,7 @@ namespace CNS {
             c.saveperiod = savePeriod;
             c.PrintInterval = 1;
 
-            c.WriteLTSLog = true;
+            c.WriteLTSLog = false;
             c.forceReclustering = false;
 
             // ### Partitioning and load balancing ###
