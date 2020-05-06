772a77741d3054ea72055ec23314dccb36acdc04
Lauritz Beck <lauritzbeck@gmail.com>
2020-05-06 19:17:44 +0200

    OneTimeSetup

--- src/L4-application/XdgTimesteppingTest/TestProgram.cs
+++ src/L4-application/XdgTimesteppingTest/TestProgram.cs
@@ -30,7 +30,12 @@ namespace BoSSS.Application.XdgTimesteppingTest {
     [TestFixture]
     class TestProgram {
 
-       
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            BoSSS.Solution.Application.InitMPI();
+        }
+
         /// <summary>
         /// Tests the <see cref="BoSSS.Solution.XdgTimestepping.XdgBDFTimestepping"/> time-stepper at 
         /// polynomial order 0 with single-value init, see <see cref="BoSSS.Solution.XdgTimestepping.XdgBDFTimestepping.SingleInit"/>.
