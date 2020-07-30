3e1bee0003dae222331f76407c70dd2edb94ed80
JFGut <gutierrez@fdy.tu-darmstadt.de>
2020-07-30 22:59:17 +0200

    Added support for variable parameters of the OneStep model depending on the equivalence ratio (or mixture fraction)

--- src/L3-solution/BoSSS.Solution.NSECommon/MaterialLawCombustion.cs
+++ src/L3-solution/BoSSS.Solution.NSECommon/MaterialLawCombustion.cs
@@ -22,6 +22,7 @@ using BoSSS.Foundation;
 using System.Diagnostics;
 using ilPSP.Utils;
 using System.Runtime.Serialization;
+using ilPSP;
 
 namespace BoSSS.Solution.NSECommon {
 
@@ -31,26 +32,49 @@ namespace BoSSS.Solution.NSECommon {
     /// Material law for low Mach number flows with combustion number.
     /// </summary>
     public class MaterialLawCombustion : MaterialLawLowMach {
-        [DataMember]
-        MaterialParamsMode MatParamsMode;
-        [DataMember]
-        double[] MolarMasses;
-        [DataMember]
-        bool rhoOne;
-  
+        [DataMember] MaterialParamsMode MatParamsMode;
+        [DataMember] double[] MolarMasses;
+        [DataMember] bool rhoOne;
+        [DataMember] public double Q;
+        [DataMember] public double TO0;
+        [DataMember] public double TF0;
+        [DataMember] public double YF0;
+        [DataMember] public double YO0;
+        [DataMember] public double zst;
+        [DataMember] public double cp;
+        [DataMember] public ChemicalConstants CC;
+        public double s;
         /// <summary>
         /// Ctor.
         /// </summary>
         /// <param name="T_ref">Reference temperature - used in Sutherland's law.</param>
         /// <param name="MatParamsMode">The selected material parameter mode.</param>
+        /// <param name="rhoOne"></param>
+        /// <param name="Q"></param>
+        /// <param name="TO0"></param>
+        /// <param name="TF0"></param>
+        /// <param name="YO0"></param>
+        /// <param name="YF0"></param>
+        /// <param name="zst"></param>
+        /// <param name="CC"></param>
+        /// <param name="Prandtl"></param>
         /// <param name="MolarMasses">Array of the molar masses of the fuel, oxidizer and products.</param>
-        public MaterialLawCombustion(double T_ref, double[] MolarMasses, bool rhoOne, MaterialParamsMode MatParamsMode, double Prandtl)
+        public MaterialLawCombustion(double T_ref, double[] MolarMasses,  MaterialParamsMode MatParamsMode, bool rhoOne, double Q, double TO0, double TF0, double YO0, double YF0, double zst, ChemicalConstants CC, double Prandtl)
             : base(T_ref, MatParamsMode, rhoOne, Prandtl) {
             this.MatParamsMode = MatParamsMode;
             this.Prandtl = Prandtl;
             this.MolarMasses = MolarMasses;
             this.rhoOne = rhoOne;
-           
+            this.Q = Q;
+            this.TO0 = TO0;
+            this.TF0 = TF0;
+            this.YF0 = YF0;
+            this.YO0 = YO0;
+            this.zst = zst;
+            this.cp = 1.0;
+            this.CC = CC;
+            this.s = (CC.nu_O2 * CC.MW_O2) / (CC.nu_CH4 * CC.MW_CH4);
+
         }
 
 
@@ -68,8 +92,8 @@ namespace BoSSS.Solution.NSECommon {
                 double rho;
                 double MassFractionsOverMolarFractions;
                 if (!rhoOne) {
-                    if (phi.Length < 4)
-                        throw new ArgumentException("Error in density computation. Number of reactants needs to be atleast 3.");
+                    if (phi.Length < 5)
+                        throw new ArgumentException("Error in density computation. Number of reactants needs to be atleast 4.");
 
                     MassFractionsOverMolarFractions = 0.0;
                     double lastMassFract = 1.0; // The last mass fraction is calculated here, because the other ones are given as arguments and not as parameters.
@@ -89,8 +113,6 @@ namespace BoSSS.Solution.NSECommon {
                 } else {
                     rho = 1.0;
                 }
-
-
                 return rho;
             } else {
                 throw new ApplicationException("ThermodynamicPressure is not initialized.");
@@ -148,6 +170,95 @@ namespace BoSSS.Solution.NSECommon {
             cp *= R / avgMW; // cp in KJ/Kg K
             return cp; // 
         }
+
+
+        /// <summary>
+        /// Calculates local mixture fraction
+        /// </summary>     
+        /// <returns></returns>
+        public double getMixtureFraction(double YF, double YO ) {
+            double Z = (s * YF - YO + YO0) / (s * YF0 + YO0);
+            return Z;
+        }
+
+        /// <summary>
+        /// Calculates the global equivalence ratio 
+        /// </summary>     
+        /// <returns></returns>
+        public double getGlobalEquivalenceRatio(double Yf0, double Yox0) {
+            Debug.Assert(!(Yf0 < 0));
+            Debug.Assert(!(Yox0 < 0));
+            double phi = s * Yf0 / Yox0;
+            return phi;
+        }
+
+
+
+        /// <summary>
+        /// Calculates the global equivalence ratio 
+        /// </summary>     
+        /// <returns></returns>
+        public double getLocalEquivalenceRatio(double Yf, double Yox) {
+            Debug.Assert(!(Yf < 0));
+            Debug.Assert(!(Yox < 0));
+            double Z = getMixtureFraction(Yf, Yox);
+            double phi = s * (YF0 / YO0) * (Z / (1.0 - Z));
+            Debug.Assert(phi >= 0);
+            if (phi.IsNaNorInf()) {
+                phi = double.MaxValue;
+            }
+            return phi;
+        }
+
+        /// <summary>
+        /// Calculates local activation energy based on the one-Step model from Fernandez Tarrazo, E, A Sanchez, A Linan, and F Williams. “A Simple One-Step Chemistry Model for 
+        /// Partially Premixed Hydrocarbon Combustion.” Combustion and Flame 147, no. 1–2 (October 2006): 32–38. https://doi.org/10.1016/j.combustflame.2006.08.001.
+        /// </summary>
+        /// <param name="Yf"></param>
+        /// <param name="Yox"></param>
+        /// <returns></returns>
+        public double getTa(double Yf, double Yox) {
+            double phi = getLocalEquivalenceRatio(Yf, Yox);
+            double Ta;
+            if (phi <= 0.64) {
+                phi = phi < 0 ? 0.0 : phi;
+                Ta = 1.0 + 8.250 * (phi - 0.64) * (phi - 0.64);
+            } else if (phi > 1.07) {
+                //phi = phi < 0 ? 0.0 : phi;
+                Ta = 1.0 + 1.443 * (phi - 1.07) * (phi - 1.07);
+            } else {
+                Ta = 1.0;
+            }
+            double Ta0 = 15900; 
+            Ta *= Ta0;
+            return Ta;
+        }
+
+
+
+
+        /// <summary>
+        /// Calculates local heat release based on the one-Step model from Fernandez Tarrazo, E, A Sanchez, A Linan, and F Williams. “A Simple One-Step Chemistry Model for 
+        /// Partially Premixed Hydrocarbon Combustion.” Combustion and Flame 147, no. 1–2 (October 2006): 32–38. https://doi.org/10.1016/j.combustflame.2006.08.001.
+        /// </summary>
+        /// <param name="Yf"></param>
+        /// <param name="Yox"></param>
+        /// <returns></returns>
+        public double getHeatRelease(double Yf, double Yox) {
+            double phi = getLocalEquivalenceRatio(Yf, Yox);
+            double q;
+            double q0 = 50100; // Heat release per KG fuel, [kJ/kg]
+            if (phi <= 1.0) {
+                q = 1.0;
+            } else {
+                q = 1.0 - 0.21 * (phi - 1);
+            }
+            q *= q0;
+
+         //   q = q < 0 ? 0.0 : q;// Accept only positive or zero value for q.
+            return q;
+        }
+
 
 
 
