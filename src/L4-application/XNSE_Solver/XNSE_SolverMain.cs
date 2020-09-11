ac84f3fa5f00c3e0f6d566ed803164f3eb6f0fe0
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2020-09-11 19:58:19 +0200

    bgfx

--- src/L4-application/XNSE_Solver/XNSE_SolverMain.cs
+++ src/L4-application/XNSE_Solver/XNSE_SolverMain.cs
@@ -73,12 +73,12 @@ namespace BoSSS.Application.XNSE_Solver {
 
         static void Main(string[] args) {
 
-            InitMPI();
+            //InitMPI();
             //DeleteOldPlotFiles();
             //BoSSS.Application.XNSE_Solver.Tests.UnitTest.ChannelTest(2, 0.0d, ViscosityMode.Standard, 0.0d);
             //BoSSS.Application.XNSE_Solver.Tests.UnitTest.BcTest_PressureOutletTest(1, 0.0d, true);
-            BoSSS.Application.XNSE_Solver.Tests.UnitTest.ScalingViscosityJumpTest_p3(ViscosityMode.FullySymmetric);
-            throw new Exception("fuck you ");
+            //BoSSS.Application.XNSE_Solver.Tests.UnitTest.ScalingViscosityJumpTest_p3(ViscosityMode.FullySymmetric);
+            //throw new Exception("fuck you ");
 
 
             _Main(args, false, delegate () {
