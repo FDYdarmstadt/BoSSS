1b088894ad088148e8bea29619a87664a0ac5f7d
JFGut <gutierrez@fdy.tu-darmstadt.de>
2020-09-16 16:38:20 +0200

    bug fix

--- src/L3-solution/BoSSS.Solution.NSECommon/Divergence_CentralDifference.cs
+++ src/L3-solution/BoSSS.Solution.NSECommon/Divergence_CentralDifference.cs
@@ -63,10 +63,10 @@ namespace BoSSS.Solution.NSECommon {
                 case PhysicsMode.Multiphase:
                     throw new ApplicationException("Wrong constructor");
                 case PhysicsMode.LowMach:
-                    this.m_ParamterOrdering = null; //  new string[] { VariableNames.Temperature0 };
+                    this.m_ParamterOrdering =new string[] { VariableNames.Temperature0 };
                     break;
                 case PhysicsMode.Combustion:
-                    this.m_ParamterOrdering = null; /* = ArrayTools.Cat(new string[] { VariableNames.Temperature0 }, VariableNames.MassFractions0(NumberOfReactants));*/
+                    this.m_ParamterOrdering =  ArrayTools.Cat(new string[] { VariableNames.Temperature0 }, VariableNames.MassFractions0(NumberOfReactants));
                     this.NumberOfReactants = NumberOfReactants;
                     if(NumberOfReactants == -1)
                         throw new ArgumentException("NumberOfReactants must be specified for combustion flows.");
