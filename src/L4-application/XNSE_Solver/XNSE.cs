f326acfe6064648e426d6e67c3e6deffe8727044
Lauritz Beck <beck@fdy.tu-darmstadt.de>
2020-12-09 14:04:28 +0100

    removed redundant comments

--- src/L4-application/XNSE_Solver/XNSE.cs
+++ src/L4-application/XNSE_Solver/XNSE.cs
@@ -129,8 +129,6 @@ namespace BoSSS.Application.XNSE_Solver
             LevelSet levelSet = new LevelSet(new Basis(GridData, levelSetDegree), "Phi");
             levelSet.ProjectField(Control.InitialValues_Evaluators["Phi"]);
 
-            //levelSet = new SplineLevelSet(x => (0.01 * Math.Sin(x * 2 * Math.PI)), new Basis(GridData, levelSetDegree), "Phi", 200);
-            //Tecplot.PlotFields(new DGField[] { levelSet }, "levelSet", 0, 3);
             switch (Control.Option_LevelSetEvolution)
             {
                 case LevelSetEvolution.Fourier:
@@ -143,9 +141,6 @@ namespace BoSSS.Application.XNSE_Solver
                     break;
                 case LevelSetEvolution.FastMarching:
                     lsUpdater = new LevelSetUpdater((GridData)GridData, Control.CutCellQuadratureType, 1, new string[] { "A", "B" }, levelSet);
-                    //var spliner = new SplineLevelSetEvolver("Phi", (GridData)levelSet.GridDat);
-                    //lsUpdater.AddEvolver("Phi", spliner);
-
                     var fastMarcher = new FastMarcher("Phi", QuadOrder(), levelSet.GridDat.SpatialDimension);
                     lsUpdater.AddEvolver("Phi", fastMarcher);
                     lsUpdater.AddLevelSetParameter("Phi", new LevelSetVelocity("Phi", GridData.SpatialDimension, VelocityDegree(), Control.InterVelocAverage, Control.PhysicalParameters));
