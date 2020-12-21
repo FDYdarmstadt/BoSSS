a6b7a75dfa0e9b73ecb37e7c22937ef0299eacaa
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2020-12-21 04:52:28 +0100

    b4 mrg

--- src/L4-application/CNS/Program.cs
+++ src/L4-application/CNS/Program.cs
@@ -49,11 +49,12 @@ namespace CNS {
         /// <param name="args"></param>
         static void Main(string[] args) {
 
-            //Application.InitMPI(args);
+            Application.InitMPI(args);
             ////CNS.Tests.MovingIBMTests.PistonTests.MovingMeshIBMPiston1stOrderWithAgglomeration();
             //CNS.Tests.ArtificialViscosity.ArtificialViscosityShockTubeTests.ToroTest1_ALTS1_3();
-            //Debug.Assert(false, "remove me");
-            //return;
+            CNS.Tests.BoundaryConditions.EulerBoundaryConditionTest.TestSupersonicInletCondition1D();
+            Debug.Assert(false, "remove me");
+            return;
 
             Application<CNSControl>._Main(
                 args,
