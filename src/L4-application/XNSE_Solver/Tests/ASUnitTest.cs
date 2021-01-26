2a85210a445ebafb5d8f4629245625959a1eecd3
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2021-01-26 07:44:23 +0100

    .

--- src/L4-application/XNSE_Solver/Tests/ASUnitTest.cs
+++ src/L4-application/XNSE_Solver/Tests/ASUnitTest.cs
@@ -493,8 +493,8 @@ namespace BoSSS.Application.XNSE_Solver.Tests {
                 CS[i] = C;
 
                 //Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!1   remove me !!!!!!!!!!!!!!!!!!!!!!1");
-                C.ImmediatePlotPeriod = 1;
-                C.SuperSampling = 3;
+                //C.ImmediatePlotPeriod = 1;
+                //C.SuperSampling = 3;
                 //C.SkipSolveAndEvaluateResidual = true;
                 //XNSESolverTest(Tst, C);
                 //break;
--- src/L4-application/XNSE_Solver/XNSE_SolverMain.cs
+++ src/L4-application/XNSE_Solver/XNSE_SolverMain.cs
@@ -74,12 +74,12 @@ namespace BoSSS.Application.XNSE_Solver {
         //===========
         static void Main(string[] args) {
 
-            InitMPI();
-            DeleteOldPlotFiles();
-            BoSSS.Application.XNSE_Solver.Tests.ASUnitTest.TaylorCouetteConvergenceTest();
+            //InitMPI();
+            //DeleteOldPlotFiles();
+            //BoSSS.Application.XNSE_Solver.Tests.ASUnitTest.TaylorCouetteConvergenceTest();
             //BoSSS.Application.XNSE_Solver.Tests.ASUnitTest.IBMChannelTest();
             //BoSSS.Application.XNSE_Solver.Tests.ASUnitTest.BasicThreePhaseTest();
-            throw new Exception("Remove me");
+            //throw new Exception("Remove me");
 
             MultiphaseCellAgglomerator.Katastrophenplot = KatastrophenPlot;
             _Main(args, false, delegate () {
