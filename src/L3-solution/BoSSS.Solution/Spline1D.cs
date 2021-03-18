9ae75c73d86ae5ae155418fb3bb94e283238d01f
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2021-03-18 17:09:58 +0100

    bgfx

--- src/L3-solution/BoSSS.Solution/Spline1D.cs
+++ src/L3-solution/BoSSS.Solution/Spline1D.cs
@@ -252,8 +252,8 @@ namespace BoSSS.Solution.Control {
             return spline;
         }
 
-        [NonSerialized]
-        int binS_mem = -1;
+        //[NonSerialized]
+        //int binS_mem = -1;
 
         /// <summary>
         /// this function finds the interval which x belongs to
@@ -265,8 +265,8 @@ namespace BoSSS.Solution.Control {
         /// To which interval this node belongs taking into account the interpolation points
         /// </returns>
         private int binarySearch(double x) {
-            if(binS_mem >= 0 && this.nodes[binS_mem + 1] >= x && this.nodes[binS_mem] < x)
-                return binS_mem;
+            //if(binS_mem >= 0 && this.nodes[binS_mem + 1] >= x && this.nodes[binS_mem] < x)
+            //    return binS_mem;
 
             int min = 0;
             int max = this.nodes.Length - 1;
@@ -281,10 +281,10 @@ namespace BoSSS.Solution.Control {
             }
             while (mid > 0 && min <= max && (!(this.nodes[mid] >= x && this.nodes[mid - 1] < x)) && (!(this.nodes[mid] < x && this.nodes[mid + 1] >= x)));
             if (mid > 0 && this.nodes[mid] >= x && this.nodes[mid - 1] < x) {
-                binS_mem = mid - 1;
+                //binS_mem = mid - 1;
                 return (mid - 1);
             }
-            binS_mem = mid;
+            //binS_mem = mid;
             return mid;
         }
 
