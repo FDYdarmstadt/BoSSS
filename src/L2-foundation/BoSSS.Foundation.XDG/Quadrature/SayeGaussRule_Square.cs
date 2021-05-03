bcb3f6f2ceea381fbb89c7d931947866b18f4a1c
Benjamin Deußen <deussen@fdy.tu-darmstadt.de>
2021-05-03 09:56:08 +0200

    reset _square

--- src/L2-foundation/BoSSS.Foundation.XDG/Quadrature/SayeGaussRule_Square.cs
+++ src/L2-foundation/BoSSS.Foundation.XDG/Quadrature/SayeGaussRule_Square.cs
@@ -213,7 +213,7 @@ namespace BoSSS.Foundation.XDG.Quadrature
                 }
                 sum /= Math.Pow(gradient[heightDirection] - delta[heightDirection], 2);
 
-                suitable &= sum < 2;
+                suitable &= sum < 20;
 
                 return suitable;
             }
