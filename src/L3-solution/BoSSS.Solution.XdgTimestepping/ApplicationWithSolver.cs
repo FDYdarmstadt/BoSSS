69f5053cb117345687aa2662185ebb17f6ffea04
Juan Gutierrez <gutierrez@fdy.tu-darmstadt.de>
2021-07-29 10:25:53 +0200

    .

--- src/L3-solution/BoSSS.Solution.XdgTimestepping/ApplicationWithSolver.cs
+++ src/L3-solution/BoSSS.Solution.XdgTimestepping/ApplicationWithSolver.cs
@@ -153,7 +153,7 @@ namespace BoSSS.Solution.XdgTimestepping {
             var resFields = InstantiateResidualFields();
             CurrentResidualVector = new CoordinateVector(resFields);
             foreach(var f in resFields) {
-                base.RegisterField(f);
+                base.RegisterField(f/*,IOListOption.Always*/);
             }
 
             CreateAdditionalFields();
