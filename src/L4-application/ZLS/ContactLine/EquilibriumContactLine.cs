6824a7192db6cb4ade09a0079e50a2850c7f2262
Lauritz Beck <beck@fdy.tu-darmstadt.de>
2021-08-06 15:43:11 +0200

    Weird merge fix

--- src/L4-application/ZLS/ContactLine/EquilibriumContactLine.cs
+++ src/L4-application/ZLS/ContactLine/EquilibriumContactLine.cs
@@ -26,4 +26,24 @@ namespace ZwoLevelSetSolver.ContactLine {
                 .Cat(BoSSS.Solution.NSECommon.VariableNames.MaxSigma));
         }
     }
+
+    class SlipContactLine : SurfaceEquation {
+        string codomainName;
+
+        public override string FirstSpeciesName => "A";
+
+        public override string SecondSpeciesName => "B";
+
+        public override string CodomainName => codomainName;
+
+        public SlipContactLine(int d, int D, double beta, double theta) {
+            codomainName = BoSSS.Solution.NSECommon.EquationNames.MomentumEquationComponent(d);
+            AddContactLineComponent(new LaplaceBeltramiSlipForm(d, D));
+
+            AddVariableNames(BoSSS.Solution.NSECommon.VariableNames.VelocityVector(D));
+            AddParameter(BoSSS.Solution.NSECommon.VariableNames.NormalVector(D)
+                .Cat(BoSSS.Solution.NSECommon.VariableNames.AsLevelSetVariable(ZwoLevelSetSolver.VariableNames.SolidLevelSetCG, BoSSS.Solution.NSECommon.VariableNames.NormalVector(D)))
+                .Cat(BoSSS.Solution.NSECommon.VariableNames.MaxSigma));
+        }
+    }
 }
--- src/L4-application/ZLS/ContactLine/LaplaceBeltramiEquilibriumForm.cs
+++ src/L4-application/ZLS/ContactLine/LaplaceBeltramiEquilibriumForm.cs
@@ -30,4 +30,28 @@ namespace ZwoLevelSetSolver.ContactLine {
             return Flx_InCell * V;
         }
     }
+
+    class LaplaceBeltramiSlipForm : ContactLineForm {
+        int d;
+        int D;
+
+        public LaplaceBeltramiSlipForm(int d, int D) : base(D) {
+            this.d = d;
+            this.D = D;
+        }
+
+        public override double VolumeForm(ref CommonParamsVol cpv, double[] U, double[,] GradU, double V, double[] GradV) {
+            Vector EdgeNormal = SolidSurfaceNormal(ref cpv);
+            Vector SurfaceNormal_IN = FluidSurfaceNormal(ref cpv);
+
+            Vector Tangente_IN = Tangent(SurfaceNormal_IN, EdgeNormal);
+
+            double Flx_InCell = 0;
+            double m_sigma = Sigma(ref cpv);
+
+            // isotropic surface tension terms
+            Flx_InCell -= m_sigma * EdgeNormal[d] * (Tangente_IN * EdgeNormal) * (-1);
+            return Flx_InCell * V;
+        }
+    }
 }
