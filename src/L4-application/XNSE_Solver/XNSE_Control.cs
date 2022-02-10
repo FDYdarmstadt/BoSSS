eb3f1afb30b2e32596d46da8932fa136050a323f
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2022-02-10 10:47:11 +0100

    fixed Run__Channel

--- src/L4-application/XNSE_Solver/XNSE_Control.cs
+++ src/L4-application/XNSE_Solver/XNSE_Control.cs
@@ -66,7 +66,7 @@ namespace BoSSS.Application.XNSE_Solver {
             base.NonLinearSolver.MaxSolverIterations = 2000; //Solver_MaxIterations
             base.NonLinearSolver.MinSolverIterations = 4; //Solver_MinIterations
             base.NonLinearSolver.ConvergenceCriterion = 0.0; //Solver_ConvergenceCriterion: solve as accurate as possible. Don't change this, Grüße von FK!
-            base.NonLinearSolver.SolverCode = NonLinearSolverCode.Picard; //NonLinearSolver
+            base.NonLinearSolver.SolverCode = NonLinearSolverCode.Newton; //NonLinearSolver
             base.TimesteppingMode = AppControl._TimesteppingMode.Steady;
         }
 
