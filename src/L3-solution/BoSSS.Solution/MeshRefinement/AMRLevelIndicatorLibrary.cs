9e15ae83c52b97f31ec24fe81cf88ed9c92a23a0
Matthias Rieckmann <Matthias.Rieckmann@fdy.tu-darmstadt.de>
2022-03-08 11:50:29 +0100

    Hanging Nodes Multiphase Test Blueprint

--- src/L3-solution/BoSSS.Solution/MeshRefinement/AMRLevelIndicatorLibrary.cs
+++ src/L3-solution/BoSSS.Solution/MeshRefinement/AMRLevelIndicatorLibrary.cs
@@ -16,6 +16,7 @@ limitations under the License.
 
 using BoSSS.Foundation.Grid.Classic;
 using BoSSS.Foundation.XDG;
+using BoSSS.Platform.Utils.Geom;
 using ilPSP;
 using ilPSP.Utils;
 using Newtonsoft.Json;
@@ -75,6 +76,59 @@ namespace BoSSS.Solution {
                 if (!base.Equals(obj))
                     return false;
                 if (!m_EdgeTags.SetEquals((obj as AMRonBoundary)?.m_EdgeTags))
+                    return false;
+                return true;
+            }
+
+            public override int GetHashCode() {
+                return base.GetHashCode();
+            }
+        }
+
+        /// <summary>
+        /// refinement on cells which are on the specified boundary
+        /// </summary>
+        [Serializable]
+        public class AMRInBoundingBox : AMRLevelIndicator {
+
+            private BoundingBox bb;
+            public AMRInBoundingBox(BoundingBox _bb) {
+                bb = _bb;
+            }
+
+            public override int[] DesiredCellChanges() {
+
+                int J = GridData.CellPartitioning.LocalLength;
+                int[] levels = new int[J];
+                int cellsToRefine = 0;
+                int cellsToCoarse = 0;
+                Cell[] cells = GridData.Grid.Cells;
+                for (int j = 0; j < J; j++) {
+                    int currentLevel = cells[j].RefinementLevel;
+
+
+                    bool refine = false;
+                    foreach (var cell in cells) {
+                        if (bb.Contains(GridData.Cells.CellCenter.ExtractSubArrayShallow(j, -1).To1DArray()))
+                            refine = true;
+                    }
+
+                    if (refine && currentLevel < maxRefinementLevel) {
+                        levels[j] = 1;
+                        cellsToRefine++;
+                    } else if (!refine && currentLevel > 0) {
+                        levels[j] = -1;
+                        cellsToCoarse++;
+                    }
+                }
+
+                return levels;
+            }
+
+            public override bool Equals(object obj) {
+                if (!base.Equals(obj))
+                    return false;
+                if (!bb.Equals((obj as AMRInBoundingBox)?.bb))
                     return false;
                 return true;
             }
---
+++ src/L4-application/HangingNodesTests/Program.cs
@@ -0,0 +1,95 @@
+﻿using BoSSS.Application.XNSFE_Solver;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using BoSSS.Foundation.IO;
+using MPI.Wrappers;
+using System.IO;
+
+namespace HangingNodesTests {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Starting Hanging Nodes Test!");
+            BoSSS.Solution.Application.InitMPI();
+
+            //double[] sizes = new double[] { 1e0, 1e-3 };
+            //byte[] setup = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            //int[] phases = new int[] { 1, 2, 3 };
+
+            // to test individual setups
+            double[] sizes = new double[] { 1e0 };
+            byte[] setup = new byte[] { 0 };
+            int[] phases = new int[] { 2 };
+
+            bool plot = true;
+
+            csMPI.Raw.Comm_Size(MPI.Wrappers.csMPI.Raw._COMM.WORLD, out int procs);
+            csMPI.Raw.Comm_Rank(MPI.Wrappers.csMPI.Raw._COMM.WORLD, out int rank);
+
+            if (rank == 0) {
+                var dir = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+                Console.Write("rm");
+                foreach (var pltFile in dir.GetFiles("*.txt").Concat(dir.GetFiles("*.csv"))) {
+                    Console.Write(" " + pltFile.Name);
+                    pltFile.Delete();
+                }
+                Console.WriteLine(";");
+                Console.Write("rm");
+                foreach (var pltFile in dir.GetFiles("*.plt")) {
+                    Console.Write(" " + pltFile.Name);
+                    pltFile.Delete();
+                }
+                Console.WriteLine(";");
+            }
+
+
+
+            List<double> TemperatureRes = new List<double>();
+            List<double> MomentumRes = new List<double>();
+            List<string> Description = new List<string>();
+
+            foreach (double size in sizes) {
+                foreach(int phase in phases) {
+                    foreach(byte s in setup) {
+                        string desc = String.Format("Size : {0}, Phases : {1}, Setup : {2}, Procs : {3}", size, phase, s, procs);
+                        Description.Add(desc);
+                        var C = HangingNodesTests.Control.TestSkeleton(size);
+                        HangingNodesTests.Control.SetAMR(C, size, s);
+                        HangingNodesTests.Control.SetLevelSet(C, size, phase);
+                        HangingNodesTests.Control.SetParallel(C, procs);
+
+                        if (plot) {
+                            C.ImmediatePlotPeriod = 1;
+                            C.SuperSampling = 3;
+                        }
+
+                        using (var solver = new XNSFE()) {
+                            try {
+                                solver.Init(C);
+                                solver.RunSolverMode();
+                                MomentumRes.Add(solver.CurrentResidual.Fields.Take(3).Sum(f => f.L2Norm()).MPISum());
+                                TemperatureRes.Add(solver.CurrentResidual.Fields[3].L2Norm().MPISum());
+                            } catch (Exception e) {
+                                Console.WriteLine(desc + " : failed");
+                                Console.WriteLine(e.Message);
+                                Console.WriteLine(e.StackTrace);
+                                TemperatureRes.Add(-1.0);
+                                MomentumRes.Add(-1.0);
+                            }
+                        }
+                       
+                    }
+                }
+            }
+
+            BoSSS.Solution.Application.FinalizeMPI();
+            Console.WriteLine("Finished Hanging Nodes Test.");
+            Console.WriteLine();
+            Console.WriteLine("Results:");
+            for(int i = 0; i < Description.Count; i++) {
+                Console.WriteLine(Description[i] + " : MomRes : {0}, TempRes : {1}", MomentumRes[i], TemperatureRes[i]);
+            }
+        }
+    }
+}
