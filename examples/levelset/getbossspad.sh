9863be80e6bbf73d7a7da5ce3b1902410a317651
rieckmann <rieckmann@fdy.tu-darmstadt.de>
2022-07-20 11:30:21 +0200

    saving

--- examples/levelset/getbossspad.sh
+++ examples/levelset/getbossspad.sh
@@ -1,2 +1,2 @@
-dotnet publish ../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o .
-dotnet publish ../../src/L4-application/LsTest/LsTest.csproj -c Release -v q -o .
+dotnet publish ../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./binaries
+dotnet publish ../../src/L4-application/LsTest/LsTest.csproj -c Release -v q -o ./binaries
