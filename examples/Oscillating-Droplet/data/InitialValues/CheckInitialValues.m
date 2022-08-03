b4e02841b928966f749c1f1b66bea4b30cdf4706
Martin Smuda <smuda@fdy.tu-darmstadt.de>
2022-08-03 16:14:09 +0200

    further work on OD3D post-processing and producing data for publication

--- examples/Oscillating-Droplet/data/InitialValues/CheckInitialValues.m
+++ examples/Oscillating-Droplet/data/InitialValues/CheckInitialValues.m
@@ -2,19 +2,17 @@ clear all
 
 %% m3
 
-P3=@(A,eta, theta) A + eta*(1/2)*(5*((cos(theta))^3)-(3*(cos(theta))));
+P3=@(A,eta, theta) A(theta) + eta.*(1/2).*(5.*((cos(theta)).^3)-(3.*(cos(theta))));
 
-A1 = 0.996470; eta1 = 0.15; 
-A2 = 0.984618; eta2 = 0.3; 
-A3 = 0.971158; eta3 = 0.4; 
-A4 = 0.977143; eta4 = 0.4; 
+A1 =@(theta) 0.996786 - 0.000315584.*cos(theta); eta1 = 0.15; 
+A2 =@(theta) 0.987143 - 0.00252468.*cos(theta); eta2 = 0.3; 
+A3 =@(theta) 0.977143 - 0.00598442.*cos(theta); eta3 = 0.4; 
 
 %% input data
 
 dat1 = importdata('m3/surfaceDrop_m3_Oh01_eta015.txt');
 dat2 = importdata('m3/surfaceDrop_m3_Oh01_eta03.txt');
-dat3 = importdata('m3/surfaceDrop_m3_Oh01_eta04_new.txt');
-dat4 = importdata('m3/surfaceDrop_m3_Oh01_eta04.txt');      % previous data from first study
+dat3 = importdata('m3/surfaceDrop_m3_Oh01_eta04.txt');    
 
 %% check data
 
@@ -51,17 +49,6 @@ for i=1:length(dat3)
 end
 sqrt(errNorm3)
 
-errNorm4 = 0;
-expr4 = zeros(length(dat4),2);
-for i=1:length(dat4)
-    expr4(i,1) = dat4(i,1);
-    radiusDat = dat4(i,2);
-    radiusExpr = P3(A4,eta4,dat4(i,1));
-    expr4(i,2) = radiusExpr;
-    errNorm4 = errNorm4 + (radiusExpr - radiusDat)^2;
-end
-sqrt(errNorm4)
-
 
 %% plot data
 
@@ -83,32 +70,41 @@ xlabel('theta')
 ylabel('radius')
 title('m=3 eta=0.4')
 
-figure
-plot(dat4(:,1),dat4(:,2),expr4(:,1),expr4(:,2))
-xlabel('theta')
-ylabel('radius')
-title('m=3 eta=0.4')
-
 
 
 %% error plot
 
-figure
-plot(dat1(:,1),dat1(:,2) - expr1(:,2), '-', dat1(:,1), 0.5*(dat1(end,2) - expr1(end,2)) + 0.5*(dat1(end,2) - expr1(end,2)).*cos(dat1(:,1)+pi), '-.')
-xlabel('theta')
-ylabel('error')
-title('m=3 eta=0.15')
+% figure
+% plot(dat1(:,1),dat1(:,2) - expr1(:,2), '-') %, dat1(:,1), 0.5*(dat1(end,2) - expr1(end,2)) + 0.5*(dat1(end,2) - expr1(end,2)).*cos(dat1(:,1)+pi), '-.')
+% xlabel('theta')
+% ylabel('error')
+% title('m=3 eta=0.15')
+% 
+% figure
+% plot(dat2(:,1),dat2(:,2) - expr2(:,2), '-') %, dat2(:,1), 0.5*(dat2(end,2) - expr2(end,2)) + 0.5*(dat2(end,2) - expr2(end,2)).*cos(dat2(:,1)+pi), '-.')
+% xlabel('theta')
+% ylabel('error')
+% title('m=3 eta=0.3')
+% 
+% figure
+% plot(dat3(:,1),dat3(:,2) - expr3(:,2), '-') %, dat3(:,1), 0.5*(dat3(end,2) - expr3(end,2)) + 0.5*(dat3(end,2) - expr3(end,2)).*cos(dat3(:,1)+pi), '-.')
+% xlabel('theta')
+% ylabel('error')
+% title('m=3 eta=0.4')
 
-figure
-plot(dat2(:,1),dat2(:,2) - expr2(:,2), '-', dat2(:,1), 0.5*(dat2(end,2) - expr2(end,2)) + 0.5*(dat2(end,2) - expr2(end,2)).*cos(dat2(:,1)+pi), '-.')
-xlabel('theta')
-ylabel('error')
-title('m=3 eta=0.3')
 
-figure
-plot(dat3(:,1),dat3(:,2) - expr3(:,2), '-', dat3(:,1), 0.5*(dat3(end,2) - expr3(end,2)) + 0.5*(dat3(end,2) - expr3(end,2)).*cos(dat3(:,1)+pi), '-.')
-xlabel('theta')
-ylabel('error')
-title('m=3 eta=0.4')
+%% check center of mass position
+
+P3_vol1 =@(theta) 0.5*P3(A1, eta1, theta).^2; 
+P3_vol2 =@(theta) 0.5*P3(A2, eta2, theta).^2; 
+P3_vol3 =@(theta) 0.5*P3(A3, eta3, theta).^2; 
+
+volume1 = integral(P3_vol1, 0, pi);
+volume2 = integral(P3_vol2, 0, pi);
+volume3 = integral(P3_vol3, 0, pi);
+
+P3_cy1 =@(theta) (1/3).*cos(theta).*(P3(A1, eta1, theta).^3);
+
+centery = integral(P3_cy1, 0, pi)/volume1
 
 
