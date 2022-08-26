58eb3e64d015863f1531d4f9125fc560c4ad56de
Dario <dario.klingenberg@web.de>
2022-08-26 15:15:36 +0200

    add cahnhilliard stuff to test folder

---
+++ src/L4-application/ExternalBinding/CahnHilliardTest.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using BoSSS.Foundation.Grid;
+
+namespace BoSSS.Application.ExternalBinding {
+
+    static public class CahnHilliardTest {
+
+        public static void Main() {
+
+            Init();
+            var chOp = new FixedOperators();
+            OpenFOAMGrid grd = GridImportTest.GenerateFOAMGrid();
+            OpenFoamDGField f = new(grd, 2, 2);
+            OpenFoamMatrix mtx = new(grd, f);
+            // var EdgeValues = new List<List<double>>();
+            // foreach (var val in new double[]{1, -1, 0}){
+            //     EdgeValues.Add(new List<double>{val});
+            // }
+            // OpenFoamPatchField cPtch = new(grd, 1, new int[]{1,2,3}, new string[]{"dirichlet","dirichlet","neumann"}, new double[]{1,-1,0});
+            OpenFoamPatchField cPtch;
+            unsafe {
+                int[] safeEts = new[]{1, 2, 3};
+                // int* ets = (int*)safeEts[0];
+                int[] safeEtyps = new[]{1, 1, 0};
+                // int* eTyps = (int*)safeEtyps[0];
+                double[] safeVals = new[]{1.0, -1.0, 0};
+                fixed (double* eVals = safeVals){
+                    fixed (int* ets = safeEts)
+                    {
+                        fixed (int* eTyps = safeEtyps)
+                        {
+                            cPtch = new(grd, 1, 3, ets, eTyps, eVals);
+                        }
+                    }
+                }
+            }
+            chOp.CahnHilliard(mtx, null, cPtch, null);
+            Cleanup();
+
+        }
+
+        static Initializer MyInit;
+
+        /// <summary>
+        /// MPI Init
+        /// </summary>
+        public static void Init() {
+            MyInit = new Initializer();
+            MyInit.BoSSSInitialize();
+        }
+
+        /// <summary>
+        /// MPI shutdown
+        /// </summary>
+        public static void Cleanup() {
+            MyInit.BoSSSFinalize();
+        }
+    }
+}
