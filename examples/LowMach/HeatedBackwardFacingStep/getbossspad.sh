75e2a660d9b7b4426f762bde6d844ccfaa02a07d
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2022-09-16 15:17:39 +0200

    hgf

--- examples/LowMach/HeatedBackwardFacingStep/getbossspad.sh
+++ examples/LowMach/HeatedBackwardFacingStep/getbossspad.sh
@@ -1,2 +1,2 @@
-dotnet publish ../../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o . > build.txt
+dotnet publish ../../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o . > build.out
 echo $?
