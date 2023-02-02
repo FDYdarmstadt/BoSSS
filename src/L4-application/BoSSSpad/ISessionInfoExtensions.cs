d740046875b70f4213821d825b530967005f2234
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2023-02-02 05:09:37 +0100

    b4 merge

--- src/L4-application/BoSSSpad/ISessionInfoExtensions.cs
+++ src/L4-application/BoSSSpad/ISessionInfoExtensions.cs
@@ -2563,14 +2563,7 @@ namespace BoSSS.Foundation.IO {
         /// </summary>
         static public Plot2Ddata GetMPItotalMemory(this ISessionInfo sess) {
             var ana = new SessionMemtrace(new DirectoryInfo(sess.GetSessionDirectory()));
-            int L = ana.NoOfTimeEntries;
-
-            var ret = new Plot2Ddata();
-
-            ret.AddDataGroup(new XYvalues(
-                $"Tot Mem [MegB] at {ana.MPIsize} cores",
-                L.ForLoop(i => (double)i),
-                ana.TotalMemMegs));
+            var ret = ana.GetMPItotalMemory();
 
             ret.Title = "Total memory of session " + sess;
 
@@ -2579,37 +2572,22 @@ namespace BoSSS.Foundation.IO {
         }
 
         /// <summary>
-        /// total memory (aka. sum) over all MPI ranks over time
+        /// minimum, average and maximum memory allocations over all MPI ranks over time
         /// </summary>
-        static public Plot2Ddata GetMPIMemory(this ISessionInfo sess) {
+        static public Plot2Ddata GetMinAvgMaxMemory(this ISessionInfo sess) {
             var ana = new SessionMemtrace(new DirectoryInfo(sess.GetSessionDirectory()));
-            int L = ana.NoOfTimeEntries;
-
-            var ret = new Plot2Ddata();
-
-            ret.AddDataGroup(new XYvalues(
-                $"Min Mem [MegB] at {ana.MPIsize} cores",
-                L.ForLoop(i => (double)i),
-                ana.MinimumMemMegs));
-
-            ret.AddDataGroup(new XYvalues(
-                $"Max Mem [MegB] at {ana.MPIsize} cores",
-                L.ForLoop(i => (double)i),
-                ana.MaximumMemMeg));
-
-            ret.AddDataGroup(new XYvalues(
-                $"Avg Mem [MegB] at {ana.MPIsize} cores",
-                L.ForLoop(i => (double)i),
-                ana.AverageMemMeg));
-
-            ret.Title = "Memory of session " + sess;
-
-
+            var ret = ana.GetMinAvgMaxMemPlot();
+            ret.Title = "Memory of session " + sess.ID;
             return ret;
         }
 
-        static SessionMemtrace GetMemtrace(This ISessionInfo) {
-            return SessionMemtrace(new DirectoryInfo(sess.GetSessionDirectory()));
+        /// <summary>
+        /// Returns the memory instrumentation for a session (if available),
+        /// combined from files `memory.mpi_rank.txt` in the session directory
+        /// </summary>
+        public static SessionMemtrace GetMemtrace(this ISessionInfo sess) {
+            var ret = new SessionMemtrace(new DirectoryInfo(sess.GetSessionDirectory()));
+            return ret;
         }
 
 
