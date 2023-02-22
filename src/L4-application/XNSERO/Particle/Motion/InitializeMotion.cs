9bcd4335d82d9610f9693802720bddad53231157
Benjamin Deußen <deussen@fdy.tu-darmstadt.de>
2023-02-22 11:59:34 +0100

    removed added damping from InitMotion (no advantage versus normal motion calc)

--- src/L4-application/XNSERO/Particle/Motion/InitializeMotion.cs
+++ src/L4-application/XNSERO/Particle/Motion/InitializeMotion.cs
@@ -25,9 +25,6 @@ namespace BoSSS.Application.XNSERO_Solver {
         /// <summary>
         /// The initialization of the particle motion. Depending on the given parameters the correct model is chosen.
         /// </summary>
-        /// <param name="gravity">
-        /// The gravity (volume forces) acting on the particle.
-        /// </param>
         /// <param name="particleDensity">
         /// The density of the particle.
         /// </param>
@@ -36,23 +33,17 @@ namespace BoSSS.Application.XNSERO_Solver {
         /// </param>
         /// <param name="noRotation"></param>
         /// <param name="noTranslation"></param>
-        /// <param name="underrelaxationParam">
-        /// The underrelaxation parameters (convergence limit, prefactor and a bool whether to use addaptive underrelaxation) defined in <see cref="ParticleUnderrelaxationParam"/>.
-        /// </param>
-        /// <param name="addedDampingCoefficient">
-        /// The added damping coefficient is a scaling factor for the model. If the value is smaller than zero no added damping is applied. Otherwise it should be between 0.5 and 1.5, for reference: Banks et.al. 2017.
-        /// </param>
-        public InitializeMotion(double particleDensity = 0, bool isDry = false, bool noRotation = false, bool noTranslation = false, double addedDampingCoefficient = 0, bool calculateOnlyVelocity = false) {
-            this.particleDensity = particleDensity == 0 ? 1 : particleDensity;
+        public InitializeMotion(double particleDensity = 1, bool isDry = false, bool noRotation = false, bool noTranslation = false, bool calculateOnlyVelocity = false) {
+            this.particleDensity = particleDensity;
             this.isDry = isDry;
             this.noRotation = noRotation;
             this.noTranslation = noTranslation;
-            this.addedDampingCoefficient = addedDampingCoefficient;
             this.calculateOnlyVelocity = calculateOnlyVelocity;
+
+            if (particleDensity == 0)
+                throw new Exception("Zero particle density is not allowed.");
         }
 
-        [DataMember]
-        private readonly Auxillary Aux = new Auxillary();
         [DataMember]
         private readonly double particleDensity;
         [DataMember]
@@ -63,19 +54,6 @@ namespace BoSSS.Application.XNSERO_Solver {
         private readonly bool noTranslation;
         [DataMember]
         private readonly bool calculateOnlyVelocity;
-        [DataMember]
-        private readonly double addedDampingCoefficient;
-
-        /// <summary>
-        /// Initial check for the particle motion parameter.
-        /// </summary>
-        public void CheckInput() {
-            if (addedDampingCoefficient != 0 && addedDampingCoefficient < 0.5 && addedDampingCoefficient > 1.5)
-                throw new Exception("Error in control file: Added damping coefficient should be between 0.5 and 1.5! See for reference Banks et al.");
-            if (addedDampingCoefficient != 0 && (noRotation || noTranslation))
-                throw new Exception("Error in control file: The added damping model is designed to contain all possible motion types (translation and rotation).");
-            Aux.TestArithmeticException(addedDampingCoefficient, "added damping coefficient");
-        }
 
         /// <summary>
         /// Initialize the correct derived motion.cs.
@@ -88,13 +66,11 @@ namespace BoSSS.Application.XNSERO_Solver {
                     else
                         return new MotionFixed();
                 }
-                if (isDry) {
+                else if (isDry) {
                     return noRotation ? new MotionDryNoRotation(particleDensity)
                         : noTranslation ? new MotionDryNoTranslation(particleDensity)
                         : new MotionDry(particleDensity);
                 }
-                if (addedDampingCoefficient != 0)
-                    return new MotionAddedDamping(particleDensity, addedDampingCoefficient);
                 else
                     return noRotation ? new MotionWetNoRotation(particleDensity)
                         : noTranslation ? new MotionWetNoTranslation(particleDensity)
