4931e956245c713b91a81975f4133ad0df57a7cd
jakob <sebastian@fdy.tu-darmstadt.de>
2023-05-17 10:09:02 +0200

    Copy pasted SpecialRules from BruteFroceScheme to BeckQuadRules

--- src/L4-application/IntersectingLevelSetTest/Program.cs
+++ src/L4-application/IntersectingLevelSetTest/Program.cs
@@ -5,15 +5,15 @@ namespace IntersectingLevelSetTest
     class Program
     {
         static void Main(string[] args)
-        {            
-            XQuadFactoryHelper.CheckQuadRules = true;
+        {
+            //XQuadFactoryHelper.CheckQuadRules = true;
             //AllUpTest.AllUp(0, XQuadFactoryHelper.MomentFittingVariants.Saye);
-            AllUpTest.LocalTestWithPlotting(2, XQuadFactoryHelper.MomentFittingVariants.Saye);
+            //AllUpTest.LocalTestWithPlotting(2, XQuadFactoryHelper.MomentFittingVariants.Saye);
 
-            //BoSSS.Solution.Application<PlotControl>._Main(
-            //    args,
-            //    true,
-            //    () => new ZwoLsCoupledSolver<PlotControl>() {MomentFittingVariant = XQuadFactoryHelper.MomentFittingVariants.Saye });
+            BoSSS.Solution.Application<PlotControl>._Main(
+                args,
+                true,
+                () => new ZwoLsCoupledSolver<PlotControl>() { MomentFittingVariant = XQuadFactoryHelper.MomentFittingVariants.Saye });
         }
     }
 }
