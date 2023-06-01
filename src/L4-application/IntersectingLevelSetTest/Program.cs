625aa776895dd9b939f38c5bf412cf364375c726
jakob <sebastian@fdy.tu-darmstadt.de>
2023-06-01 16:23:28 +0200

    Added XESPF - WIP

--- src/L4-application/IntersectingLevelSetTest/Program.cs
+++ src/L4-application/IntersectingLevelSetTest/Program.cs
@@ -6,14 +6,14 @@ namespace IntersectingLevelSetTest
     {
         static void Main(string[] args)
         {            
-            XQuadFactoryHelper.CheckQuadRules = true;
+            //XQuadFactoryHelper.CheckQuadRules = true;
             //AllUpTest.AllUp(0, XQuadFactoryHelper.MomentFittingVariants.Saye);
-            AllUpTest.LocalTestWithPlotting(2, XQuadFactoryHelper.MomentFittingVariants.Saye);
+            //AllUpTest.LocalTestWithPlotting(2, XQuadFactoryHelper.MomentFittingVariants.Saye);
 
-            //BoSSS.Solution.Application<PlotControl>._Main(
-            //    args,
-            //    true,
-            //    () => new ZwoLsCoupledSolver<PlotControl>() {MomentFittingVariant = XQuadFactoryHelper.MomentFittingVariants.Saye });
+            BoSSS.Solution.Application<PlotControl>._Main(
+                args,
+                false,
+                () => new ZwoLsCoupledSolver<PlotControl>() { MomentFittingVariant = XQuadFactoryHelper.MomentFittingVariants.Saye });
         }
     }
 }
