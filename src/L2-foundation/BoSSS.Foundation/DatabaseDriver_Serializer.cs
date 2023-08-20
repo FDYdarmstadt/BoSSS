2ac61aa30f2354a372d0c4e9597f5835437725f6
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2023-08-20 07:44:58 +0200

    IO Perf improvement

--- src/L2-foundation/BoSSS.Foundation/DatabaseDriver_Serializer.cs
+++ src/L2-foundation/BoSSS.Foundation/DatabaseDriver_Serializer.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Linq;
 using Newtonsoft.Json.Serialization;
+using System.Diagnostics;
+using System.IO.Compression;
 
 namespace BoSSS.Foundation.IO
 {
@@ -19,8 +21,7 @@ namespace BoSSS.Foundation.IO
         void Serialize(Stream stream, object obj, Type objectType);
     }
 
-    abstract class Serializer : ISerializer
-    {
+    abstract class Serializer : ISerializer {
         public abstract string Name { get; }
 
         protected abstract JsonSerializer JsonFormatter { get; }
@@ -32,49 +33,48 @@ namespace BoSSS.Foundation.IO
         /// </summary>
         protected static readonly bool DebugSerialization = false;
 
-        public virtual object Deserialize(Stream stream, Type objectType)
-        {
-            using (var reader = GetJsonReader(stream))
-            {
-                object obj = JsonFormatter.Deserialize(reader, objectType);
-                if (obj == null)
-                {
-                    throw new IOException("Deserializing failed.");
+        public virtual object Deserialize(Stream stream, Type objectType) {
+            // fk, 19aug23: Create a MemoryStream and write the buffer into it
+            // for compressed streams, the deserialization is than much faster than directly deserializing from the compressed stream
+            using (MemoryStream memoryStream = new MemoryStream()) {
+                stream.CopyTo(memoryStream);
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                using (var reader = GetJsonReader(memoryStream)) {
+                    object obj = JsonFormatter.Deserialize(reader, objectType);
+                    if (obj == null) {
+                        throw new IOException("Deserializing failed.");
+                    }
+                    return obj;
                 }
-                return obj;
             }
         }
 
-        public void Serialize(Stream s, object obj, Type objectType)
-        {
-            using (var writer = GetJsonWriter(s))
-            {
-                JsonFormatter.Serialize(writer, obj, objectType);
-                writer.Close();
-                s.Close();
+        public void Serialize(Stream _s, object obj, Type objectType) {
+            using (var s = new MemoryStream()) {
+                using (var writer = GetJsonWriter(s)) {
+                    JsonFormatter.Serialize(writer, obj, objectType);
+                    writer.Close();
+                }
+
+                s.Seek(0, SeekOrigin.Begin);
+                s.CopyTo(_s);
+
             }
         }
 
-        protected JsonReader GetJsonReader(Stream s)
-        {
-            if (DebugSerialization)
-            {
+        protected JsonReader GetJsonReader(Stream s) {
+            if (DebugSerialization) {
                 return new JsonTextReader(new StreamReader(s));
-            }
-            else
-            {
+            } else {
                 return new BsonDataReader(s);
             }
         }
 
-        protected JsonWriter GetJsonWriter(Stream s)
-        {
-            if (DebugSerialization)
-            {
+        protected JsonWriter GetJsonWriter(Stream s) {
+            if (DebugSerialization) {
                 return new JsonTextWriter(new StreamWriter(s));
-            }
-            else
-            {
+            } else {
                 return new BsonDataWriter(s);
             }
         }
@@ -91,7 +91,7 @@ namespace BoSSS.Foundation.IO
             ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
             ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
             //Binder = new MySerializationBinder()
-            SerializationBinder = new MySerializationBinder()
+            //SerializationBinder = new MySerializationBinder()
         };
 
         /// <summary>
@@ -121,24 +121,34 @@ namespace BoSSS.Foundation.IO
         }
 
         object JsonDeserialize(Stream stream, Type objectType) {
-            using (var reader = GetJsonReader(stream)) {
-                object obj = jsonFormatter.Deserialize(reader, objectType);
-                if (obj == null) {
-                    throw new Exception("Deserializing failed.");
+            // fk, 19aug23: Create a MemoryStream and write the buffer into it
+            // for compressed streams, the deserialization is than much faster than directly deserializing from the compressed stream
+            using (MemoryStream memoryStream = new MemoryStream()) {
+                stream.CopyTo(memoryStream);
+                memoryStream.Seek(0, SeekOrigin.Begin);
+           
+                using (var reader = GetJsonReader(memoryStream)) {
+
+
+
+
+                    object obj = jsonFormatter.Deserialize(reader, objectType);
+                
+                    if (obj == null) {
+                        throw new IOException("Deserializing failed; object is null");
+                    }
+                    return obj;
                 }
-                return obj;
             }
         }
     }
 
-    class SerializerVersion1 : Serializer, ISerializer
-    {
+    class SerializerVersion1 : Serializer, ISerializer {
         public override string Name => "Version 1";
 
         protected override JsonSerializer JsonFormatter => jsonFormatter;
 
-        JsonSerializer jsonFormatter = new JsonSerializer()
-        {
+        JsonSerializer jsonFormatter = new JsonSerializer() {
             NullValueHandling = NullValueHandling.Ignore,
             TypeNameHandling = TypeNameHandling.Objects,
             ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
