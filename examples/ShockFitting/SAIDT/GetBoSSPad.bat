72af091c2a44d4a3c3f2727ca4e39921b165f13b
sebastian <sebastian@fdy.tu-darmstadt.de>
2023-08-24 22:37:39 +0200

    added Notebooks for SAIDT and BUIDT

---
+++ examples/ShockFitting/SAIDT/GetBoSSPad.bat
@@ -0,0 +1,2 @@
+dotnet publish ../../../../src/L4-application/SAIDT/SAIDT.csproj -c Release -v q -o ./binaries
+dotnet publish ../../../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./binaries
