f69a92455f9b601c46010cb4539c373bbd8da48d
muhammed.toprak <toprak@fdy.tu-darmstadt.de>
2023-09-04 16:07:34 +0200

    AgglomerationKatastrophe_HighRes is turn off

--- src/L3-solution/BoSSS.Solution.LevelSetTools/SolverWithLevelSetUpdater/SolverWithLevelSetUpdater.cs
+++ src/L3-solution/BoSSS.Solution.LevelSetTools/SolverWithLevelSetUpdater/SolverWithLevelSetUpdater.cs
@@ -615,8 +615,9 @@ namespace BoSSS.Solution.LevelSetTools.SolverWithLevelSetUpdater {
                 }
 
                 Tecplot.Tecplot.PlotFields(allfields, Tag+"AgglomerationKatastrophe", 0.0, 0);
-                if (Tag.Length > 0) 
-                    Tecplot.Tecplot.PlotFields(allfields, Tag + "AgglomerationKatastrophe_HighRes", 0.0, 2);
+
+                //if (Tag.Length > 0) 
+                //    Tecplot.Tecplot.PlotFields(allfields, Tag + "AgglomerationKatastrophe_HighRes", 0.0, 2);
 
             }
             AgglomerationAlgorithm.Katastrophenplot = KatastrophenPlot;
