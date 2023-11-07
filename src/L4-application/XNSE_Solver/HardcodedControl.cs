f272b614e4a6ba911bb4e59d7da7b82bcc6a813f
muhammed.toprak <toprak@fdy.tu-darmstadt.de>
2023-11-07 09:45:26 +0100

    Added test for agglomeration threshold =0

--- src/L4-application/XNSE_Solver/HardcodedControl.cs
+++ src/L4-application/XNSE_Solver/HardcodedControl.cs
@@ -6409,7 +6409,7 @@ namespace BoSSS.Application.XNSE_Solver {
             // throws an error due to the SymPart_DiagBlockEquilib_DropIndefinite
             var C = RotatingTiltedXRigid(2, 16, 3, true, AMRLevel: 1, TiltAngle: Math.PI / 4, SolverOn: false);
             C.AgglomerationThreshold = 0.0;
-            C.NoOfTimesteps = 100;
+            C.NoOfTimesteps = 1;
             return C;
         }
 
@@ -6417,7 +6417,6 @@ namespace BoSSS.Application.XNSE_Solver {
             // throws an error due to the SymPart_DiagBlockEquilib_DropIndefinite
             var C = RotatingTiltedXRigid(1, 20, 3, true, AMRLevel: 2, TiltAngle: Math.PI/6, SolverOn: true);
             C.NoOfTimesteps = 100;
-            C.TimeSteppingScheme = TimeSteppingScheme.ExplicitEuler;
             return C;
         }
 
