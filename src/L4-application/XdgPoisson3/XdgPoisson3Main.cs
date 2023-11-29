4f032ba423c8a0ee4872841d34f41841976d5b8d
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2023-11-29 21:09:29 +0100

    added code to check multi-thread para

--- src/L4-application/XdgPoisson3/XdgPoisson3Main.cs
+++ src/L4-application/XdgPoisson3/XdgPoisson3Main.cs
@@ -168,11 +168,10 @@ namespace BoSSS.Application.XdgPoisson3 {
 
             Op = new XDifferentialOperatorMk2(1, 1, (A, B, C) => order, this.LsTrk.SpeciesNames, "u", "c1");
             Op.AgglomerationThreshold = this.Control.AgglomerationThreshold;
-            //var lengthScales = ((BoSSS.Foundation.Grid.Classic.GridData)GridData).Cells.PenaltyLengthScales;
             var lap = new XLaplace_Bulk(penalty_multiplyer, "u", this.Control.xLaplaceBCs, 1.0, MU_A, MU_B, this.Control.ViscosityMode);
             Op.EquationComponents["c1"].Add(lap);      // Bulk form
             Op.EquationComponents["c1"].Add(new XLaplace_Interface( MU_A, MU_B, penalty_multiplyer, this.Control.ViscosityMode));   // coupling form
-            //Op.EquationComponents["c1"].Add(new RHSSource(this.rhs));
+            Op.EquationComponents["c1"].Add(new RHSSource(this.rhs));
             Op.IsLinear = true;
 
             Op.Commit();
