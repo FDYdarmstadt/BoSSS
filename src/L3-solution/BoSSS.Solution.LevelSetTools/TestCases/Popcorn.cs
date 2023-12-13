2de48794805d46db037bb5377b9ecb542cd15334
muhammed.toprak <toprak@fdy.tu-darmstadt.de>
2023-12-13 09:02:05 +0100

    Popcorn testcase + improvements

--- src/L3-solution/BoSSS.Solution.LevelSetTools/TestCases/Popcorn.cs
+++ src/L3-solution/BoSSS.Solution.LevelSetTools/TestCases/Popcorn.cs
@@ -10,14 +10,14 @@ namespace BoSSS.Solution.LevelSetTools.TestCases {
         double sigma;
         double A;
 
-        public Popcorn(double radius, double sigma, double A = 2.0) {
+        public Popcorn(double radius, double sigma=0.2, double A = 2.0) {
             this.radius = radius;
             this.sigma = sigma;
             this.A = A;
         }
 
         /// <summary>
-        /// Signed-Distance Level-Set Function for a Torus
+        /// Signed-Distance Level-Set Function for a Popcorn
         /// </summary>
         /// <param name="X">Cartesian Coordinates</param>
         /// <returns>Level-Set Value at X</returns>
@@ -25,9 +25,9 @@ namespace BoSSS.Solution.LevelSetTools.TestCases {
             double SumOfSeries = 0;
             for (int i = 0; i <= 11; i++) {
                 double[] xk = Xk(i);
-                SumOfSeries += A * Math.Exp(-((X[0] - xk[0]).Pow2() + (X[1] - xk[1]).Pow2() + (X[2] - xk[2]).Pow2()) / sigma * sigma);
+                SumOfSeries += A * Math.Exp(-( (X[0] - xk[0]).Pow2() + (X[1] - xk[1]).Pow2() + (X[2] - xk[2]).Pow2() ) / sigma.Pow2());
             }
-            return Math.Sqrt(X[0].Pow2() + X[1].Pow2() + X[2].Pow2() - radius - SumOfSeries);
+            return (Math.Sqrt(X[0].Pow2() + X[1].Pow2() + X[2].Pow2()) - radius - SumOfSeries);
         }
 
 
@@ -47,11 +47,11 @@ namespace BoSSS.Solution.LevelSetTools.TestCases {
         /// <returns>Level-Set Value at X</returns>
         public double[] Xk(int k) {
             double[] Xk = new double[3];
-            if (k <= 4) {
+            if (k >= 0 && k <= 4) {
                 Xk[0] = 2 * Math.Cos(2 * k * Math.PI / 5) * radius / Math.Sqrt(5);
                 Xk[1] = 2 * Math.Sin(2 * k * Math.PI / 5) * radius / Math.Sqrt(5);
                 Xk[2] = 1 * radius / Math.Sqrt(5);
-            } else if (k <= 9) {
+            } else if (k >= 5 && k <= 9) {
                 Xk[0] = 2 * Math.Cos((2 * (k - 5) - 1) * Math.PI / 5) * radius / Math.Sqrt(5);
                 Xk[1] = 2 * Math.Sin((2 * (k - 5) - 1) * Math.PI / 5) * radius / Math.Sqrt(5);
                 Xk[2] = -1 * radius / Math.Sqrt(5);
