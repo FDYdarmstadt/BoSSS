37acc0f5c0eaff2664766aa2b8c783562b6dd784
Kummer, Florian <kummer@fdy.tu-darmstadt.de>
2024-01-18 00:24:44 +0100

    Transition to two-stage pipeline

---
+++ src/ilPSP/layer_1.2-ilPSP/ilPSP/CPUAffinity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ilPSP.Utils {
+    /// <summary>
+    /// Returns the CPU affinity, 
+    /// supporting systems with more than 64 processors (unlike <see cref="Process.ProcessorAffinity"/>).
+    /// </summary>
+    public class CPUAffinity {
+
+
+        /// <summary>
+        /// Returns the list of CPU's to which the current process is assigned to;
+        /// Driver which calls either the respective Linux or Windows API functions.
+        /// </summary>
+        public static IEnumerable<int> GetAffinity() {
+
+            //Console.WriteLine("bootstrapping necessary.");
+            if (System.Environment.OSVersion.Platform == PlatformID.Win32NT) {
+                return CPUAffinityWindows.GetAffinity();
+
+            } else if (System.Environment.OSVersion.Platform == PlatformID.Unix) {
+                return CPUAffinityLinux.GetAffinity();
+            } else {
+                throw new NotSupportedException("Not implemented for system: " + System.Environment.OSVersion.Platform);
+            }
+
+        }
+    }
+}
