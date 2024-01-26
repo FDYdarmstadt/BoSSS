1dc72b6671c544d00d4727d2a2e18a8cb631cc51
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2024-01-26 08:31:05 +0100

    fix

--- src/ilPSP/layer_1.2-ilPSP/ilPSP/Environment.cs
+++ src/ilPSP/layer_1.2-ilPSP/ilPSP/Environment.cs
@@ -520,7 +520,7 @@ namespace ilPSP {
                 double maxFactor = TimeX.maxTime/TimeRef.maxTime;
 
 
-                if (minFactor.MPIMax() > 5 || maxFactor.MPIMax() > 5 || avgFactor.MPIMax() > 5) {
+                if (minFactor.MPIMax() > 5 || maxFactor.MPIMax() > 7 || avgFactor.MPIMax() > 12) {
 
                     string scaling = $"R{Rank}: {ranksToBench+1} workers: (min|avg|max) : (\t{TimeX.minTime:0.###E-00} |\t{TimeX.avgTime:0.###E-00} |\t{TimeX.maxTime:0.###E-00})  --- \t\t( {minFactor:0.##E-00} |\t{avgFactor:0.###E-00} |\t{maxFactor:0.##E-00})";
                     Console.WriteLine("Benchmarking error: " + scaling);
