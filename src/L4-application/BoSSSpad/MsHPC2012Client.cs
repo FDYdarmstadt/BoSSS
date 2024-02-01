08ec1162e6141c23cf246825b83c7d6ac3f091d2
rieckmann <rieckmann@fdy.tu-darmstadt.de>
2024-02-01 11:16:32 +0100

    Extensionmethod for submitting multi taks mshpc jobs

--- src/L4-application/BoSSSpad/MsHPC2012Client.cs
+++ src/L4-application/BoSSSpad/MsHPC2012Client.cs
@@ -525,7 +525,7 @@ namespace BoSSS.Application.BoSSSpad {
             return ret;
         }
 
-
+       
         /// <summary>
         /// Submits the job to the Microsoft HPC server.
         /// </summary>
@@ -557,7 +557,7 @@ namespace BoSSS.Application.BoSSSpad {
                     for (int i = 0; i < parts.Length; i++) {
                         if (parts[i].Equals("id:", StringComparison.InvariantCultureIgnoreCase))
                             id = int.Parse(parts[i + 1]);
-
+                            
                     }
                     //Console.WriteLine(ret);
                 }
@@ -815,6 +815,16 @@ namespace BoSSS.Application.BoSSSpad {
             get {
                 return m_AdditionalEnvironmentVars;
             }
+        }
+
+
+        /// <summary>
+        /// publicly available wrapper, used for extension methods to <see cref="MsHPC2012Client"/>
+        /// </summary>
+        /// <param name="xmlFilePath"></param>
+        /// <returns></returns>
+        public (int exitcode, string stdOut, string stdErr) SubmitProcess(string xmlFilePath) {
+            return this.ExecuteProcess("job.exe", $"submit /jobfile:\"{xmlFilePath}\" {this.GetLoginArg()}", 60000);
         }
 
 
