992a40934daf02b25b7033d8515ae0480b60af82
sebastian <sebastian@fdy.tu-darmstadt.de>
2024-03-30 14:57:36 +0100

    some documaentation for the studies

--- examples/ShockFitting/1DSpcTmEuler/GetBoSSPad.bat
+++
@@ -1,2 +0,0 @@
-dotnet publish ../../XESTSF/XESTSF.csproj -c Release -v q -o ./binaries
-dotnet publish ../../../../public/src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./binaries
---
+++ examples/ShockFitting/Studies/GetBoSSPad.bat
@@ -0,0 +1,5 @@
+dotnet publish ../../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./ConvergenceStudyBowShock/binaries
+dotnet publish ../../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./ComparisonShockCapturing/binaries
+dotnet publish ../../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./ComparisonOptimizationProblem/binaries
+dotnet publish ../../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./ComparisonLevelSets/binaries
+dotnet publish ../../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./ComparisonFluxes/binaries
