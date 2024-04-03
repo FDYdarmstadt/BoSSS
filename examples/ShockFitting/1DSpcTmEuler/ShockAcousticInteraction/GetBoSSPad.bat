4eefdfbb7d66e320dd2ebfc315964bbb24beb276
sebastian <sebastian@fdy.tu-darmstadt.de>
2024-04-03 07:47:46 +0200

    minor notebook stuff

--- examples/ShockFitting/1DSpcTmEuler/ShockAcousticInteraction/GetBoSSPad.bat
+++ examples/ShockFitting/1DSpcTmEuler/ShockAcousticInteraction/GetBoSSPad.bat
@@ -1,2 +1,1 @@
-dotnet publish ../../../../src/L4-application/XESTSF/XESTSF.csproj -c Release -v q -o ./binaries
 dotnet publish ../../../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./binaries
