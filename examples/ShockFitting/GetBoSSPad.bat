59f0eff4c6bb1d987bd6d3211534bfa94df91459
sebastian <sebastian@fdy.tu-darmstadt.de>
2024-04-03 10:32:43 +0200

    added agglomerator update after accumulatestep

---
+++ examples/ShockFitting/GetBoSSPad.bat
@@ -0,0 +1,6 @@
+dotnet publish ../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./1DSpcTmAdvection/binaries
+dotnet publish ../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./1DSpcTmBrgs/AccShock/binaries
+dotnet publish ../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./1DSpcTmBrgs/StraigthShock/binaries
+dotnet publish ../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./1DSpcTmEuler/ShockAcousticInteraction/binaries
+dotnet publish ../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./2DSteadyEuler/BowShock/binaries
+dotnet publish ../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -v q -o ./2DSteadyEuler/WedgeFlow/binaries
