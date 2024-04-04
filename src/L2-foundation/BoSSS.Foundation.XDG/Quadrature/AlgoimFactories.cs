71b7b021bb8c0d50110d0ac96394c9b0bed4cff8
muhammed.toprak <toprak@fdy.tu-darmstadt.de>
2024-04-04 09:03:33 +0200

    Improved the wrapper

--- src/L2-foundation/BoSSS.Foundation.XDG/Quadrature/AlgoimFactories.cs
+++ src/L2-foundation/BoSSS.Foundation.XDG/Quadrature/AlgoimFactories.cs
@@ -1,16 +1,53 @@
-﻿using System;
+﻿using BoSSS.Foundation.Grid;
+using BoSSS.Foundation.Grid.RefElements;
+using BoSSS.Foundation.Quadrature;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
+using static BoSSS.Foundation.XDG.Quadrature.HMF.LineSegment;
 
-namespace BoSSS.Foundation.XDG.Quadrature
-{
-    public static class AlgoimFactories
-    {
+namespace BoSSS.Foundation.XDG.Quadrature {
+    public static class AlgoimFactories {
         #region Edge rules
 
 
+        public static IQuadRuleFactory<CellBoundaryQuadRule> AlgoimGaussRule_EdgeVolume3D(
+            LevelSetTracker.LevelSetData _lsData,
+            IRootFindingAlgorithm RootFinder) {
+
+            ISayeGaussEdgeRule rule = new SayeGaussRule_EdgeCube(
+                _lsData,
+                RootFinder,
+                SayeGaussRule_Cube.QuadratureMode.PositiveVolume);
+            return new SayeGaussEdgeRuleFactory(rule);
+        }
+
         #endregion
-
-
     }
+
+    //QuadRule RuleToRuleThemAll = QuadRule.CreateEmpty(refElement, count, spatialDim);
+    //RuleToRuleThemAll.Nodes = new NodeSet(refElement, nodes, true);
+    //RuleToRuleThemAll.Weights = weights;
+    //        return RuleToRuleThemAll;
+
+
+
+    class AlgoimEdgeRuleFactory : IQuadRuleFactory<CellBoundaryQuadRule> {
+        public RefElement RefElement => throw new NotImplementedException();
+
+
+        public AlgoimEdgeRuleFactory() { }
+
+        public int[] GetCachedRuleOrders() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IChunkRulePair<CellBoundaryQuadRule>> GetQuadRuleSet(ExecutionMask mask, int order) {
+            throw new NotImplementedException();
+        }
+    }
+
 }
+
+
