5574608b6fe8e3c39a6cc68020fa4efe10c741f9
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2024-04-30 09:50:51 +0200

    polishing

--- src/ilPSP/layer_1.2-ilPSP/ilPSP/OnlinePerformanceMeasurement.cs
+++ src/ilPSP/layer_1.2-ilPSP/ilPSP/OnlinePerformanceMeasurement.cs
@@ -37,13 +37,15 @@ namespace ilPSP {
             foreach(var br in BenchResults) {
                 tw.Write(br.Key);
                 tw.Write(": ");
-                double[] res = br.Value.ToArray();
-                double avg = res.Sum()/res.Count();
-                double min = res.Min();
-                double max = res.Max();
-                tw.Write($"[{min:g5} | {avg:g5} | {max:g5}]");
-                tw.Write("  ");
-                tw.Write(res.ToConcatString("[", ", ", "]", "g4"));
+                double[] res = br.Value.Where(x => x > 0).ToArray();
+                if (res.Length > 0) {
+                    double avg = res.Sum()/res.Count();
+                    double min = res.Min();
+                    double max = res.Max();
+                    tw.Write($"[{min:g5} | {avg:g5} | {max:g5}]");
+                    tw.Write("  ");
+                }
+                tw.Write(br.Value.ToConcatString("[", ", ", "]", "g4"));
                 tw.WriteLine();
             }
         }
