b0b6de1a8a4f4e366285f6fefad8fa6333c33aac
muhammed.toprak <toprak@fdy.tu-darmstadt.de>
2024-06-19 13:48:37 +0200

    test: added testing algoim for CutEdgeQuadrature

--- src/L4-application/CutEdgeQuadrature/Program.cs
+++ src/L4-application/CutEdgeQuadrature/Program.cs
@@ -15,11 +15,18 @@ namespace CutEdgeQuadrature {
         static void Main(string[] args) {
             Application.InitMPI();
             Tester3D tester = new Tester3D();
+                
             foreach(var test in tests3D) {
-                int order = 3;
-                double error = tester.Test(test, order);
-                Console.WriteLine($"Order: {order}, Error: {error}");
+                test.MomentFittingVariant = BoSSS.Foundation.XDG.XQuadFactoryHelperBase.MomentFittingVariants.Algoim;
+                Console.WriteLine($"### Testing: {test.GetType()} ###");
+                int[] orders = new int[] { 1,2,3,4};
+
+                foreach (int order in orders) { 
+                    double error = tester.Test(test, order);
+                    Console.WriteLine($"Order: {order}, Error: {error}");
+                }
             }
+
             Console.WriteLine("Press any key...");
             Console.ReadKey();
         }
