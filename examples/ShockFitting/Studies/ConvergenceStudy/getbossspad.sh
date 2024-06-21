f0d0dc9216fa0f75b44157a67bdce991f3b7c5c9
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2024-06-21 13:15:49 +0200

    ws fix

---
+++ examples/ShockFitting/Studies/ConvergenceStudy/getbossspad.sh
@@ -0,0 +1,3 @@
+dotnet publish ../../../../src/L4-application/BoSSSpad/BoSSSpad.csproj -c Release -o . > out.sh
+dotnet publish ../../../../src/L4-application/XESF/XESF.csproj -c Release -o . > out2.sh
+echo $?
