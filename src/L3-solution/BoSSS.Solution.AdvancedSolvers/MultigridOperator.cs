24030cae2582b87fa717470723daeef0f426a98a
muhammed.toprak <toprak@fdy.tu-darmstadt.de>
2025-01-13 16:04:14 +0100

    feat: make raw operator matrix public

--- src/L3-solution/BoSSS.Solution.AdvancedSolvers/MultigridOperator.cs
+++ src/L3-solution/BoSSS.Solution.AdvancedSolvers/MultigridOperator.cs
@@ -564,7 +564,7 @@ namespace BoSSS.Solution.AdvancedSolvers {
             }
         }
 
-        BlockMsrMatrix m_RawOperatorMatrix = null; // forgotten after Setup()
+        public BlockMsrMatrix m_RawOperatorMatrix = null; // forgotten after Setup() public
         BlockMsrMatrix m_RawMassMatrix = null;
 
         bool setupdone = false;
