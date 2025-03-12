d4548536b8dc02578656b49641cd45d9962d1f04
Matthias Rieckmann <Matthias.Rieckmann@fdy.tu-darmstadt.de>
2025-03-12 17:01:26 +0100

    parameter fix

--- src/L3-solution/BoSSS.Solution.XNSECommon/Equations.cs
+++ src/L3-solution/BoSSS.Solution.XNSECommon/Equations.cs
@@ -542,7 +542,6 @@ namespace BoSSS.Solution.XNSECommon {
                     AddComponent(new CurvatureBasedSurfaceTension(d, D, sigma));                    
                     AddParameter(BoSSS.Solution.NSECommon.VariableNames.Curvature);
                     AddParameter(BoSSS.Solution.NSECommon.VariableNames.NormalVector(D)[d]);
-                    AddParameter(BoSSS.Solution.NSECommon.VariableNames.LevelSetGradient(D)[d]);
 
                     // == Implementation with SurfaceElementOperator
                     //AddSurfaceComponent(new CurvatureBasedSurfaceTension_SurfaceOperator(d, D, sigma));
