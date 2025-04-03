19e81f8e3c67873c7fae6285b4f0bbb9dd99e112
Tristan Schulz <tschulz@fdy.maschinenbau.tu-darmstadt.de>
2025-04-03 14:25:36 +0200

    remove automatic include

--- src/L4-application/BoSSSpad/MsHPC2012Client.cs
+++ src/L4-application/BoSSSpad/MsHPC2012Client.cs
@@ -21,7 +21,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Management.Automation;
 using System.Runtime.Serialization;
 
 namespace BoSSS.Application.BoSSSpad {
