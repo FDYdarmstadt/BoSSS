80283210c988647bbe6f00a408f5e7b1a3465dc5
muhammed.toprak <toprak@fdy.tu-darmstadt.de>
2025-10-24 15:07:28 +0200

    feat: added a new project for multithread testing

---
+++ src/L4-application/MultiThreadingTest/MultiThreadingTestMain.cs
@@ -0,0 +1,22 @@
+﻿namespace MultiThreadingTest {
+
+    public class MultiThreadingTestMain {
+        static void Main(string[] args) {
+            Console.WriteLine("Given input parameters: ");
+            foreach(var arg in args) {
+                Console.Write(arg);
+            }
+
+            BoSSS.Solution.Application.InitMPI(args);
+            Console.WriteLine("Starting multithread tests");
+            TPLtest.SimpleTestTask(1000);
+
+            TPLtest.ParTestSeq(1000, 1000);
+
+            TPLtest.ParTestOMP(1000, 1000);
+            MPI.Wrappers.csMPI.Raw.mpiFinalize();
+
+        }
+    }
+
+}
