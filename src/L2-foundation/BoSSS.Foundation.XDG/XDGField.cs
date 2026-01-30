c341113574c4f9065bc8c1b10e045be33cdc484a
Paul Hollmann <hollmann@fdy.tu-darmstadt.de>
2026-01-30 17:00:36 +0100

    Update XDGField.cs

--- src/L2-foundation/BoSSS.Foundation.XDG/XDGField.cs
+++ src/L2-foundation/BoSSS.Foundation.XDG/XDGField.cs
@@ -162,7 +162,7 @@ namespace BoSSS.Foundation.XDG {
         /// return value is cached
         /// </remarks>
         public SpeciesShadowField GetSpeciesShadowField(SpeciesId id) {
-            MPICollectiveWatchDog.Watch();
+            //MPICollectiveWatchDog.Watch();
             if (!m_SpeciesShadowFields.ContainsKey(id.cntnt)) {
                 SpeciesShadowField ssf = new SpeciesShadowField(this, id);
                 m_SpeciesShadowFields.Add(id.cntnt, ssf);
