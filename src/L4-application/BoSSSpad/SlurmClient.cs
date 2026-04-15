5d7921d8698372a52d1be7e0f2e50368f9b8e953
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2026-04-15 07:48:25 +0200

    compile fix

--- src/L4-application/BoSSSpad/SlurmClient.cs
+++ src/L4-application/BoSSSpad/SlurmClient.cs
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-using Azure.Identity;
+//using Azure.Identity;
 using ilPSP;
 using ilPSP.Tracing;
 using System;
